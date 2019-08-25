507d9917840371e14422369c301a7b2f9656c7ee
Ray Redondo <rdrpenguin04@users.noreply.github.com>
2019-08-25 11:47:12 -0500

    Start adding instructions to 65C816_EMU
    Added all mnemonics and first 0x20 addressing modes

--- DiztinGUIsh/static/diz/CPU65C816_EMU.cs
+++ DiztinGUIsh/static/diz/CPU65C816_EMU.cs
@@ -105,17 +105,44 @@ namespace DiztinGUIsh
         
         private enum AddressMode : byte
         {
-        
+            IMPLIED, ACCUMULATOR, CONSTANT, IMMEDIATE,
+            ZERO_PAGE, ZERO_PAGE_X_INDEX, ZERO_PAGE_Y_INDEX,
+            ZERO_PAGE_INDIRECT, ZERO_PAGE_X_INDEX_INDIRECT, ZERO_PAGE_INDIRECT_Y_INDEX,
+            ZERO_PAGE_AND_RELATIVE
+            ADDRESS, ADDRESS_X_INDEX, ADDRESS_Y_INDEX,
+            RELATIVE
         }
         
         private static string[] mnemonics =
         {
-        
+            "BRK", "ORA", "COP", "NOP", "TSB", "ORA", "ASL", "RMB0", "PHP", "ORA", "ASL", "NOP", "TSB", "ORA", "ASL", "BBR0",
+            "BPL", "ORA", "ORA", "NOP", "TRB", "ORA", "ASL", "RMB1", "CLC", "ORA", "INC", "NOP", "TRB", "ORA", "ASL", "BBR1",
+            "JSR", "AND", "NOP", "NOP", "BIT", "AND", "ROL", "RMB2", "PLP", "AND", "ROL", "NOP", "BIT", "AND", "ROL", "BBR2",
+            "BPL", "AND", "AND", "NOP", "BIT", "AND", "ROL", "RMB3", "SEC", "AND", "DEC", "NOP", "BIT", "AND", "ROL", "BBR3",
+            "RTI", "EOR", "NOP", "NOP", "NOP", "EOR", "LSR", "RMB4", "PHA", "EOR", "LSR", "NOP", "JMP", "EOR", "LSR", "BBR4",
+            "BVC", "EOR", "EOR", "NOP", "NOP", "EOR", "LSR", "RMB5", "CLI", "EOR", "PHY", "NOP", "NOP", "EOR", "LSR", "BBR5",
+            "RTS", "ADC", "NOP", "NOP", "STZ", "ADC", "ROR", "RMB6", "PLA", "ADC", "ROR", "NOP", "JMP", "ADC", "ROR", "BBR6",
+            "BVS", "ADC", "ADC", "NOP", "STZ", "ADC", "ROR", "RMB7", "SEI", "ADC", "PLY", "NOP", "JMP", "ADC", "ROR", "BBR7",
+            "BRA", "STA", "NOP", "NOP", "STY", "STA", "STX", "SMB0", "DEY", "BIT", "TXA", "NOP", "STY", "STA", "STX", "BBS0",
+            "BCC", "STA", "STA", "NOP", "STY", "STA", "STX", "SMB1", "TYA", "STA", "TXS", "NOP", "STZ", "STA", "STZ", "BBS1",
+            "LDY", "LDA", "NOP", "NOP", "LDY", "LDA", "LDX", "SMB2", "TAY", "LDA", "TAX", "NOP", "LDY", "LDA", "LDX", "BBS2",
+            "BCS", "LDA", "LDA", "NOP", "LDY", "LDA", "LDX", "SMB3", "CLV", "LDA", "TSX", "NOP", "LDY", "LDA", "LDX", "BBS3",
+            "CPY", "CMP", "NOP", "NOP", "CPY", "CMP", "DEC", "SMB4", "INY", "CMP", "DEX", "WAI", "CPY", "CMP", "DEC", "BBS4",
+            "BNE", "CMP", "CMP", "NOP", "NOP", "CMP", "DEC", "SMB5", "CLD", "CMP", "PHX", "STP", "NOP", "CMP", "DEC", "BBS5",
+            "CPX", "SBC", "NOP", "NOP", "CPX", "SBC", "INC", "SMB6", "INX", "SBC", "NOP", "NOP", "CPX", "SBC", "INC", "BBS6",
+            "BEQ", "SBC", "SBC", "NOP", "NOP", "SBC", "INC", "SMB7", "SED", "SBC", "PLX", "XCE", "NOP", "SBC", "INC", "BBS7"
         };
         
         private static AddressMode[] addressingModes =
         {
-        
+            AddressMode.CONSTANT, AddressMode.ZERO_PAGE_X_INDEX_INDIRECT, AddressMode.CONSTANT, AddressMode.IMPLIED,
+            AddressMode.ZERO_PAGE, AddressMode.ZERO_PAGE, AddressMode.ZERO_PAGE, AddressMode.ZERO_PAGE,
+            AddressMode.IMPLIED, AddressMode.IMMEDIATE, AddressMode.ACCUMULATOR, AddressMode.IMPLIED,
+            AddressMode.ADDRESS, AddressMode.ADDRESS, AddressMode.ADDRESS, AddressMode.ZERO_PAGE_AND_RELATIVE,
+            AddressMode.RELATIVE, AddressMode.ZERO_PAGE_Y_INDEX_INDIRECT, AddressMode.ZERO_PAGE_INDIRECT, AddressMode.IMPLIED,
+            AddressMode.ZERO_PAGE, AddressMode.ZERO_PAGE_X_INDEX, AddressMode.ZERO_PAGE_X_INDEX, AddressMode.ZERO_PAGE,
+            AddressMode.IMPLIED, AddressMode.ADDRESS_Y_INDEX, AddressMode.ACCUMULATOR, AddressMode.IMPLIED,
+            AddressMode.ADDRESS, AddressMode.ADDRESS_X_INDEX, AddressMode.ADDRESS_X_INDEX, AddressMode.ZERO_PAGE_AND_RELATIVE,
         };
     }
 }
