f780be05c29e980b7c6ca632085dc6a2c6a2bf0d
Dominic Cerquetti <binary1230@gmail.com>
2020-09-19 15:46:19 -0400

    add new way to read lines from file that: 1) saves a little memory 2) allows read-only access to files open in other apps (like CSV files when they're still open in Excel, which currently gives us a 'file in use' error)

--- DiztinGUIsh/static/Util.cs
+++ DiztinGUIsh/static/Util.cs
@@ -424,6 +424,19 @@ namespace DiztinGUIsh
             return 0;
         }
 
+        public static IEnumerable<string> ReadLines(string path)
+        {
+            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite, 0x1000, FileOptions.SequentialScan))
+            using (var sr = new StreamReader(fs, Encoding.UTF8))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    yield return line;
+                }
+            }
+        }
+
         public static string ArchToString(Data.Architechture arch)
         {
             switch (arch)
