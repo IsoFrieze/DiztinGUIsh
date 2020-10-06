9f4fd72113761f7bdae9bad43c881d6bfc94d3e3
Dominic Cerquetti <binary1230@gmail.com>
2020-10-06 16:12:32 -0400

    working IDictionary export

--- Diz.Test/SerializerTest.cs
+++ Diz.Test/SerializerTest.cs
@@ -1,36 +1,33 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Threading;
 using System.Xml;
-using System.Xml.Linq;
 using ExtendedXmlSerializer;
 using ExtendedXmlSerializer.Configuration;
-using ExtendedXmlSerializer.ContentModel;
-using ExtendedXmlSerializer.ContentModel.Content;
-using ExtendedXmlSerializer.ContentModel.Format;
-using ExtendedXmlSerializer.Core.Specifications;
-using ExtendedXmlSerializer.ExtensionModel;
-using ExtendedXmlSerializer.ExtensionModel.Instances;
 using IX.Observable;
-using IX.Undoable;
 using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace Diz.Test
 {
     public class SerializerTest
     {
+        private readonly ITestOutputHelper testOutputHelper;
+
+        public SerializerTest(ITestOutputHelper testOutputHelper)
+        {
+            this.testOutputHelper = testOutputHelper;
+        }
+
         public class Root
         {
             public OdWrapper<int, string> ODW { get; set; } = new OdWrapper<int, string>()
             {
                 ObservableDict =
                 {
-                    {1, "test1"},
-                    {2, "test3"},
+                    {1, "Xtest1"},
+                    {2, "Xtest3"},
                 }
             };
         }
@@ -40,7 +37,7 @@ namespace Diz.Test
             public ObservableDictionary<TKey, TValue> ObservableDict { get; set; } 
                 = new ObservableDictionary<TKey, TValue>();
 
-            public Dictionary<TKey, TValue> DictToSave
+            public IDictionary DictToSave
             {
                 get => new Dictionary<TKey, TValue>(ObservableDict);
                 set
@@ -48,7 +45,7 @@ namespace Diz.Test
                     ObservableDict.Clear();
                     foreach (var item in value)
                     {
-                        ObservableDict.Add(item);
+                        ObservableDict.Add((KeyValuePair<TKey, TValue>)(item));
                     }
                 }
             }
@@ -59,8 +56,8 @@ namespace Diz.Test
             return new ConfigurationContainer()
                 .EnableImplicitlyDefinedDefaultValues()
                 .EnableMemberExceptionHandling() // debug only
-                .Type<OdWrapper<int,string>>()
-                .Member(x=>x.ObservableDict)
+                .Type<OdWrapper<int, string>>()
+                .Member(x => x.ObservableDict)
                 .Ignore()
                 .UseOptimizedNamespaces()
                 .Create();
@@ -74,25 +71,13 @@ namespace Diz.Test
             var rootElement = new Root();
 
             var xmlStr = serializer.Serialize(
-                new XmlWriterSettings {Indent = true},
+                new XmlWriterSettings() {},
                 rootElement);
 
-            // Console.WriteLine(xmlStr);
-            Assert.Equal($@"<?xml version=""1.0"" encoding=""utf-8""?>
-<SerializerTest-Root xmlns:sys=""https://extendedxmlserializer.github.io/system"" xmlns:exs=""https://extendedxmlserializer.github.io/v2"" xmlns=""clr-namespace:Diz.Test;assembly=Diz.Test"">
-  <ODW>
-    <DictToSave>
-      <sys:Item>
-        <Key>1</Key>
-        <Value>test1</Value>
-      </sys:Item>
-      <sys:Item>
-        <Key>2</Key>
-        <Value>test3</Value>
-      </sys:Item>
-    </DictToSave>
-  </ODW>
-</SerializerTest-Root>", xmlStr);
+            testOutputHelper.WriteLine(xmlStr);
+            const string expected = @"<?xml version=""1.0"" encoding=""utf-8""?><SerializerTest-Root xmlns:exs=""https://extendedxmlserializer.github.io/v2"" xmlns:sys=""https://extendedxmlserializer.github.io/system"" xmlns=""clr-namespace:Diz.Test;assembly=Diz.Test""><ODW><DictToSave exs:type=""sys:Dictionary[sys:int,sys:string]""><sys:Item><Key>1</Key><Value>Xtest1</Value></sys:Item><sys:Item><Key>2</Key><Value>Xtest3</Value></sys:Item></DictToSave></ODW></SerializerTest-Root>";
+
+            Assert.Equal(expected, xmlStr);
         }
     }
 }
