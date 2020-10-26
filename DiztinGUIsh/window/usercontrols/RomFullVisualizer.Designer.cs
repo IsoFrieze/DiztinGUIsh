dfa071a0869bac0dcf3752d5a4db6cbdb701dcc9
Dominic Cerquetti <binary1230@gmail.com>
2020-10-25 22:14:41 -0400

    fix recursive issue w/visuals forms (merge error)

--- DiztinGUIsh/window/usercontrols/RomFullVisualizer.Designer.cs
+++ DiztinGUIsh/window/usercontrols/RomFullVisualizer.Designer.cs
@@ -1,13 +1,13 @@
-﻿namespace DiztinGUIsh.window
+﻿namespace DiztinGUIsh.window.usercontrols
 {
-    partial class VisualizerForm
+    partial class RomFullVisualizer
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,103 +20,42 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.panelTop = new System.Windows.Forms.Panel();
-            this.panelLegend = new System.Windows.Forms.Panel();
-            this.bankLegend1 = new DiztinGUIsh.window.usercontrols.BankLegend();
-            this.panelBottom = new System.Windows.Forms.Panel();
-            this.romFullVisualizer1 = new DiztinGUIsh.window.usercontrols.RomFullVisualizer();
-            this.panelTop.SuspendLayout();
-            this.panelLegend.SuspendLayout();
-            this.panelBottom.SuspendLayout();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.SuspendLayout();
-            //
-            // panelTop
-            //
-            this.panelTop.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelTop.Controls.Add(this.panelLegend);
-            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTop.Location = new System.Drawing.Point(0, 0);
-            this.panelTop.Name = "panelTop";
-            this.panelTop.Size = new System.Drawing.Size(765, 154);
-            this.panelTop.TabIndex = 1;
-            //
-            // panelLegend
-            //
-            this.panelLegend.Controls.Add(this.bankLegend1);
-            this.panelLegend.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLegend.Location = new System.Drawing.Point(0, 0);
-            this.panelLegend.Name = "panelLegend";
-            this.panelLegend.Size = new System.Drawing.Size(567, 152);
-            this.panelLegend.TabIndex = 0;
-            //
-            // bankLegend1
-            //
-            this.bankLegend1.AutoScroll = true;
-            this.bankLegend1.AutoSize = true;
-            this.bankLegend1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.bankLegend1.Location = new System.Drawing.Point(0, 0);
-            this.bankLegend1.Name = "bankLegend1";
-            this.bankLegend1.Size = new System.Drawing.Size(567, 152);
-            this.bankLegend1.TabIndex = 0;
-            //
-            // panelBottom
-            //
-            this.panelBottom.AutoScroll = true;
-            this.panelBottom.AutoSize = true;
-            this.panelBottom.Controls.Add(this.romFullVisualizer1);
-            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelBottom.Location = new System.Drawing.Point(0, 154);
-            this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(765, 363);
-            this.panelBottom.TabIndex = 2;
-            //
-            // romFullVisualizer1
-            //
-            this.romFullVisualizer1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.romFullVisualizer1.Location = new System.Drawing.Point(3, 3);
-            this.romFullVisualizer1.MaximumSize = new System.Drawing.Size(100, 100);
-            this.romFullVisualizer1.Name = "romFullVisualizer1";
-            this.romFullVisualizer1.Project = null;
-            this.romFullVisualizer1.Size = new System.Drawing.Size(100, 100);
-            this.romFullVisualizer1.TabIndex = 0;
-            //
-            // VisualizerForm
-            //
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.MinimumSize = new System.Drawing.Size(100, 100);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(100, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // RomFullVisualizer
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(765, 517);
-            this.Controls.Add(this.panelBottom);
-            this.Controls.Add(this.panelTop);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
-            this.Name = "VisualizerForm";
-            this.ShowIcon = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "ROM Visualizer";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.VisualizerForm_FormClosing);
-            this.Load += new System.EventHandler(this.VisualizerForm_Load);
-            this.panelTop.ResumeLayout(false);
-            this.panelLegend.ResumeLayout(false);
-            this.panelLegend.PerformLayout();
-            this.panelBottom.ResumeLayout(false);
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(100, 100);
+            this.Name = "RomFullVisualizer";
+            this.Size = new System.Drawing.Size(100, 100);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Panel panelTop;
-        private System.Windows.Forms.Panel panelLegend;
-        private System.Windows.Forms.Panel panelBottom;
-        private usercontrols.BankLegend bankLegend1;
-        private usercontrols.RomFullVisualizer romFullVisualizer1;
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
