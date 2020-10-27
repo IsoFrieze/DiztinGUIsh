4dd75196c257619ae3818a9604dc72ea4bd56d0d
Dominic Cerquetti <binary1230@gmail.com>
2020-10-27 12:17:44 -0400

    don't save the project filename in XML

--- Diz.Core/serialization/xml_serializer/XMLSerializerSupport.cs
+++ Diz.Core/serialization/xml_serializer/XMLSerializerSupport.cs
@@ -11,11 +11,14 @@ namespace Diz.Core.serialization.xml_serializer
             // This configuration changes how parts of the data structures are serialized back/forth to XML.
             // This is using the ExtendedXmlSerializer library, which has a zillion config options and is 
             // awesome.
+            //
+            // TODO: would be cool if these were stored as attributes on the classes themselves
             return new ConfigurationContainer()
                 
                 .Type<Project>()
                 .Member(x => x.UnsavedChanges).Ignore()
-                
+                .Member(x => x.ProjectFileName).Ignore()
+
                 .Type<RomBytes>()
                 .Register().Serializer().Using(RomBytesSerializer.Default)
                 
