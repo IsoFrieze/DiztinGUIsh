26666756f8e1855aba403589b4012b4d47714946
Dominic Cerquetti <binary1230@gmail.com>
2020-10-29 03:27:45 -0400

    fix appveyor.yml, try as .bat file now

---
+++ create-final-zip.bat
@@ -0,0 +1,1 @@
+﻿7z a diztinguish.zip .\DiztinGUIsh\bin\Release\*.dll .\DiztinGUIsh\bin\Release\*.exe README.md LICENSE.md .\DiztinGUIsh\dist\*
--- create-final-zip.ps1
+++
@@ -1,2 +0,0 @@
-﻿$ErrorActionPreference = "Stop";
-7z a diztinguish.zip .\DiztinGUIsh\bin\Release\*.dll .\DiztinGUIsh\bin\Release\*.exe README.md LICENSE.md .\DiztinGUIsh\dist\*
