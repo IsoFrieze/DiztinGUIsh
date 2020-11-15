e0c931d4b7e499e5bb60280b382cca6b0a5452ca
Dominic Cerquetti <binary1230@gmail.com>
2020-11-15 10:09:08 -0500

    Add unit tests for tracelog capture - Includes a sample capture session from a real run of a 4MB HiRom game - Note: Sample binary file cuts off mid-stream, so, make sure any test code you write can deal with that (it should already be doing that) - Add code for benchmarking with BenchMarkDotnet (disabled by default) - Add lighter benchmark code with xunit (less accurate but lightweight)

---
+++ Diz.Test/TraceLogPerformanceTests.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+using BenchmarkDotNet.Analysers;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Exporters;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using Diz.Core.import;
+using Diz.Core.model;
+using Diz.Core.util;
+using Diz.Test.Utils;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Diz.Test
+{
+    public sealed class XOutLogger : ILogger
+    {
+        private readonly ITestOutputHelper _helper;
+
+        public XOutLogger(ITestOutputHelper helper)
+        {
+            _helper = helper;
+        }
+
+        public void Write(LogKind logKind, string text)
+        {
+            _helper.WriteLine(text); // not quite correct but works.
+        }
+
+        public void WriteLine()
+        {
+            _helper.WriteLine("\n");
+        }
+
+        public void WriteLine(LogKind logKind, string text)
+        {
+            _helper.WriteLine(text);
+        }
+
+        public void Flush()
+        {
+        }
+
+        public string Id { get; } = "xunitLogger";
+        public int Priority { get; }
+    }
+
+    public class TraceLogPerformanceTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public TraceLogPerformanceTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        private void RunPrintTiming(Action item)
+        {
+            var s = Stopwatch.StartNew();
+            s.Start();
+                
+            item.Invoke();
+                
+            s.Stop();
+            output.WriteLine($"runtime: {s.ElapsedMilliseconds:N0}ms");
+        }
+
+        public class ImportTraceLogStreamTestHarness
+        {
+            const string datafile = "D:\\projects\\cthack\\src\\diztinguish\\Diz.Test\\testdata\\ct-binary-tracelog7.758s-60fps-locked.bin";
+            readonly Data Data = new EmptyRom();
+            private readonly BsnesTraceLogDebugBenchmarkFileCapture capturing;
+            private readonly Stopwatch stopWatch = new Stopwatch();
+            private readonly ITestOutputHelper output;
+
+            public ImportTraceLogStreamTestHarness(ITestOutputHelper output)
+            {
+                this.output = output;
+
+                capturing = new BsnesTraceLogDebugBenchmarkFileCapture(datafile, 1)
+                {
+                    OnStart = () => { stopWatch.Reset(); stopWatch.Start(); },
+                    OnStop = () =>
+                    {
+                        stopWatch.Stop();
+                        this.output.WriteLine($"runtime: {stopWatch.ElapsedMilliseconds:N0}ms");
+                    }
+                };
+            }
+
+            [Benchmark]
+            public void Run()
+            {
+                capturing.Run(Data);
+            }
+        }
+
+        [Fact]
+        public void TestTraceLogPerformance()
+        {
+            var test = new ImportTraceLogStreamTestHarness(output);
+            test.Run();
+            // XunitBenchmark.Run<ImportTraceLogStream>(output);
+        }
+    }
+}
---
+++ Diz.Test/TraceLogTests.cs
@@ -0,0 +1,47 @@
+﻿using Diz.Core.import;
+using Xunit;
+
+namespace Diz.Test
+{
+    public static class TraceLogTests
+    {
+        [Fact]
+        public static void TestCacheLines()
+        {
+            var cache = new BsnesTraceLogImporter.CachedTraceLineTextIndex();
+
+            // example line:
+            // @"028cde rep #$30               A:0004 X:0000 Y:0004 S:1fdd D:0000 DB:02 nvmxdiZC V:133 H: 654 F:36";
+            
+            // make sure the indices are correct
+            Assert.Equal(0, cache.Addr);
+            Assert.Equal(60, cache.D);
+            Assert.Equal(68, cache.Db);
+            Assert.Equal(71, cache.FN);
+            Assert.Equal(72, cache.FV);
+            Assert.Equal(73, cache.FM);
+            Assert.Equal(74, cache.FX);
+            Assert.Equal(75, cache.FD);
+            Assert.Equal(76, cache.FI);
+            Assert.Equal(77, cache.FZ);
+            Assert.Equal(78, cache.FC);
+            Assert.Equal(cache.Flags, cache.FN);
+        }
+        
+        [Fact]
+        public static void TestParseText()
+        {
+            var sampleLine = BsnesTraceLogImporter.SampleLineText;
+
+            var modData = new BsnesTraceLogImporter.ModificationData();
+            BsnesTraceLogImporter.ParseTextLine(sampleLine, modData);
+            
+            Assert.Equal(0x28cde, modData.SnesAddress);
+            Assert.Equal(0x0000, modData.DirectPage);
+            Assert.Equal(0x02, modData.DataBank);
+            Assert.False(modData.XFlagSet);
+            Assert.False(modData.MFlagSet);
+        }
+    }
+}
+
