e02a6305c5275aed7219f693c76a362300a1a289
Dominic Cerquetti <binary1230@gmail.com>
2020-11-15 09:59:29 -0500

    Add support for BenchmarkDotNet - Can use this with Unit tests from here on out

---
+++ Diz.Test/Utils/BenchmarkXunitLogger.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using Xunit.Abstractions;
+
+namespace Diz.Test.Utils
+{
+    public static class XunitBenchmark
+    {
+        public static void Run<T>(ITestOutputHelper xUnitOutput, bool debugOK=false)
+        {
+            var logger = new XOutLogger(xUnitOutput);
+            var config = ManualConfig.Create(DefaultConfig.Instance);
+            config.AddLogger(logger);
+
+            // don't use this for capturing performance info. just for debugging harnesses.
+            // always run benchmarks in release mode.
+            if (debugOK)
+                config.Options &= ConfigOptions.DisableOptimizationsValidator;
+
+            BenchmarkRunner.Run<T>(config);
+        }
+    }
+}
