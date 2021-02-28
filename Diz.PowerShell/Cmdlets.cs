69e31aca258af7a86e0bf4e50ab0bd2bf3d42bf6
Dominic Cerquetti <binary1230@gmail.com>
2021-02-28 16:41:01 -0500

    add blank scaffolding for PowerShell interface

---
+++ Diz.PowerShell/Cmdlets.cs
@@ -0,0 +1,13 @@
+﻿using System.Management.Automation;
+
+namespace Diz.PowerShell
+{
+    [Cmdlet(VerbsLifecycle.Build, "AssemblyFiles")]
+    public class BuildAssemblyFilesCmdlet : Cmdlet
+    {
+        //[Parameter(ValueFromPipelineByPropertyName = true)]
+        // public string ProjectName { get; set; }
+
+
+    }
+}
---
+++ Diz.PowerShell/runner/debug-startup.ps1
@@ -0,0 +1,12 @@
+﻿$module = "Diz.PowerShell"
+$basepath = ".\Diz.PowerShell\bin\Debug\net5.0\"
+
+echo "Starting..."
+Import-Module "$($basepath)$($module).dll"
+pwd
+
+if ((Get-Command -module $module).Count -eq 0) { 
+  Write-Host "WARNING: Couldn't find our module. (build issue?)" -ForegroundColor red
+} else {
+  Write-Host "Ready!" -ForegroundColor green
+}
