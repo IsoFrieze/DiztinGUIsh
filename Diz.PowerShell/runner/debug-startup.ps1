38fdeaab66c90b67810d089cafcb22040ed0851b
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-05 01:30:25 -0500

    add working build command w/powershell add test script to debug it from visual studio

--- Diz.PowerShell/runner/debug-startup.ps1
+++ Diz.PowerShell/runner/debug-startup.ps1
@@ -1,12 +1,21 @@
 ﻿$module = "Diz.PowerShell"
-$basepath = ".\Diz.PowerShell\bin\Debug\net5.0\"
+$basepath = ".\"
 
 echo "Starting..."
 Import-Module "$($basepath)$($module).dll"
-pwd
+
+WRite-Host "Current working dir---> $($pwd)'"
 
 if ((Get-Command -module $module).Count -eq 0) { 
   Write-Host "WARNING: Couldn't find our module. (build issue?)" -ForegroundColor red
 } else {
   Write-Host "Ready!" -ForegroundColor green
 }
+
+$dizproject = "testproject.dizraw"
+$cmd_to_run = "Build-AssemblyFiles -ProjectNames (Resolve-Path '..\..\..\..\..\rom\$($dizproject)')"
+
+Write-Host "Press enter to run this command: '$($cmd_to_run)'"
+$null = Read-Host
+
+Invoke-Expression $cmd_to_run
