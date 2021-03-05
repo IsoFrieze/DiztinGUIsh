1fa586689d5a002c6598c812fafcf530e5a6eaf4
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-05 04:10:55 -0500

    rename str function

--- Diz.Core/util/ByteUtil.cs
+++ Diz.Core/util/ByteUtil.cs
@@ -116,10 +116,10 @@ namespace Diz.Core.util
                 (data[offset + 1] << 8);
         }
 
-        public static byte[] StringToByteArray(string s)
+        public static byte[] StringToNullTermByteArray(string s)
         {
-            byte[] array = new byte[s.Length + 1];
-            for (int i = 0; i < s.Length; i++) array[i] = (byte)s[i];
+            var array = new byte[s.Length + 1];
+            for (var i = 0; i < s.Length; i++) array[i] = (byte)s[i];
             array[s.Length] = 0;
             return array;
         }
