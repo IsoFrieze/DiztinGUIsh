556f5007bbb1e932324f1be3f8338defc278bd24
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-05 04:13:32 -0500

    resave progress bar form so it displays again correctly.

--- DiztinGUIsh/window/dialog/ProgressDialog.Designer.cs
+++ DiztinGUIsh/window/dialog/ProgressDialog.Designer.cs
@@ -27,6 +27,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProgressDialog));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
@@ -37,55 +38,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(11, 264);
+            this.label1.Location = new System.Drawing.Point(13, 305);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.Size = new System.Drawing.Size(42, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Status:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(55, 264);
+            this.label2.Location = new System.Drawing.Point(64, 305);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.Size = new System.Drawing.Size(83, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "[progress text]";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(11, 286);
+            this.progressBar1.Location = new System.Drawing.Point(13, 331);
             this.progressBar1.Margin = new System.Windows.Forms.Padding(2);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(255, 19);
+            this.progressBar1.Size = new System.Drawing.Size(298, 22);
             this.progressBar1.TabIndex = 2;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Image = global::DiztinGUIsh.Properties.Resources.diz_4;
-            this.pictureBox1.Location = new System.Drawing.Point(7, 2);
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(259, 259);
+            this.pictureBox1.Size = new System.Drawing.Size(298, 289);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // ProgressDialog
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(272, 316);
+            this.ClientSize = new System.Drawing.Size(317, 365);
             this.ControlBox = false;
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "ProgressDialog";
-            this.Padding = new System.Windows.Forms.Padding(9);
+            this.Padding = new System.Windows.Forms.Padding(10);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
--- DiztinGUIsh/window/dialog/ProgressDialog.cs
+++ DiztinGUIsh/window/dialog/ProgressDialog.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Forms;
+using DiztinGUIsh.util;
 
 namespace DiztinGUIsh.window.dialog
 {
