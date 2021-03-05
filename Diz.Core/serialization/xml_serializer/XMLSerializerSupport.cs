bd7081d347a4adb16f3cb3b4e774f51d7f1c8537
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-05 04:15:39 -0500

    messy WIP serialization support (trying to store addresses as hex not decimal in the XML)

--- Diz.Core/serialization/xml_serializer/XMLSerializerSupport.cs
+++ Diz.Core/serialization/xml_serializer/XMLSerializerSupport.cs
@@ -1,9 +1,63 @@
-﻿using Diz.Core.model;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Linq;
+using Diz.Core.model;
+using Diz.Core.util;
 using ExtendedXmlSerializer;
 using ExtendedXmlSerializer.Configuration;
+using ExtendedXmlSerializer.ContentModel.Conversion;
+using ExtendedXmlSerializer.ExtensionModel.Xml;
 
 namespace Diz.Core.serialization.xml_serializer
 {
+    /*public sealed class HexIntConverter : IConverter<int>
+    {
+        public static HexIntConverter Default { get; } = new();
+        HexIntConverter() { }
+
+        public bool IsSatisfiedBy(TypeInfo parameter) => typeof(int).GetTypeInfo()
+            .IsAssignableFrom(parameter);
+
+        public int Parse(string data)
+        {
+            return Util.ParseHexOrBase10String(data);
+        }
+
+        public string Format(int instance)
+        {
+            return instance.ToString("X");
+        }
+    }
+
+
+
+    public class HexKVPSerializer : IExtendedXmlCustomSerializer<KeyValuePair<int, string>>, IExtendedXmlCustomSerializer
+    {
+        public void Serializer(XmlWriter xmlWriter, KeyValuePair<int, string> kvp)
+        {
+            var (key, value) = kvp;
+            xmlWriter.WriteElementString("Key", key.ToString("X"));
+            xmlWriter.WriteElementString("Value", value);
+        }
+
+        KeyValuePair<int, string> IExtendedXmlCustomSerializer<KeyValuePair<int, string>>.Deserialize(XElement xElement)
+        {
+            var xElementKey = xElement.Member("Key");
+            var xElementValue = xElement.Member("Value");
+
+            if (xElementKey == null || xElementValue == null)
+                throw new InvalidOperationException("Invalid xml for class TestClassWithSerializer");
+
+            var strValue = xElement.Value;
+
+            var intValue = Util.ParseHexOrBase10String(xElementKey.Value);
+            return new KeyValuePair<int, string>(intValue, strValue);
+        }
+    }*/
+
     public static class XmlSerializerSupport
     {
         public static IConfigurationContainer GetSerializer()
@@ -18,14 +72,18 @@ namespace Diz.Core.serialization.xml_serializer
                 .Type<Project>()
                 .Member(x => x.UnsavedChanges).Ignore()
                 .Member(x => x.ProjectFileName).Ignore()
+                .Member(x => x.CurrentViewOffset).Ignore()
 
                 .Type<RomBytes>()
                 .Register().Serializer().Using(RomBytesSerializer.Default)
                 
-                .Type<Data>()
+                .Type<Data>()// .Register().Converter(HexIntConverter.Default)
+                // .Member(x => x.Comments.Keys).Register().Converter().)
+                .Member(x=>x.Comments)
+                // .CustomSerializer(new HexKVPSerializer())// cant get it working!!!
                 .UseOptimizedNamespaces()
                 .UseAutoFormatting()
-                
+
                 .EnableImplicitTyping(typeof(Data))
                 .EnableImplicitTyping(typeof(Label));
         }
