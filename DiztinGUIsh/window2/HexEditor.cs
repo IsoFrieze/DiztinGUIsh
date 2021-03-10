23950d55d2151c214ce57d46c94a4639d1dc4235
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-10 15:42:26 -0500

    add WIP hex editor (barely works) - third party control

--- DiztinGUIsh/window2/HexEditor.cs
+++ DiztinGUIsh/window2/HexEditor.cs
@@ -1,12 +1,40 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Windows.Forms;
+using Be.Windows.Forms;
+using Diz.Core.model;
 
 namespace DiztinGUIsh.window2
 {
-    public partial class HexEditor : Form
+    public interface IGarbageWhateverForm
+    {
+        public void LoadData(ArraySegment<RomByte> dataSubset);
+    }
+    
+    public partial class HexEditor : Form, IGarbageWhateverForm
     {
         public HexEditor()
         {
             InitializeComponent();
+
+            hexBox1.StringViewVisible = true;
+            hexBox1.ReadOnly = true;
+        }
+
+        private void HexEditor_Load(object sender, System.EventArgs e)
+        {
+            
+        }
+
+        public void LoadData(ArraySegment<RomByte> dataSubset)
+        {
+            var bs = new BindingSource(dataSubset.Array, "");
+
+
+            // var bytes = dataSubset.GetBytes().ToList();
+            // hexBox1.ByteProvider = new DynamicByteProvider(dataSubset.ToImmutableArray());
         }
     }
 }
