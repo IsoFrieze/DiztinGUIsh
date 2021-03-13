74de745a4bfd2e3503c4823f9aeed904a19884d9
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-12 20:58:41 -0500

    remove unused directives

--- DiztinGUIsh/window2/DataGridEditorControl.cs
+++ DiztinGUIsh/window2/DataGridEditorControl.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.Drawing;
-using System.Linq;
+﻿using System.Linq;
 using System.Windows.Forms;
 using Diz.Core.model;
 using Diz.Core.util;
