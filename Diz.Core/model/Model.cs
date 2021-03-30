39015ce6850aef28f07d0654883c957350d4df81
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-03-30 16:28:01 -0400

    - add helper interface for INotifyPropertyChanged, to deal with derived classes hookg in

--- Diz.Core/model/Model.cs
+++ Diz.Core/model/Model.cs
@@ -4,23 +4,51 @@ using System.Runtime.CompilerServices;
 
 namespace Diz.Core.model
 {
+    // makes it a little easier to deal with INotifyPropertyChanged in derived classes
+    public interface INotifyPropertyChangedExt : INotifyPropertyChanged
+    {
+        // would be great if this didn't have to be public. :shrug:
+        void OnPropertyChanged(string propertyName);
+    }
+    
     public static class NotifyPropertyChangedExtensions
     {
         // returns true if we set property to a new value
         public static bool SetField<T>(this INotifyPropertyChanged sender, PropertyChangedEventHandler handler, ref T field, T value, bool compareRefOnly = false, [CallerMemberName] string propertyName = null)
         {
+            if (!FieldCompare(field, value, compareRefOnly)) 
+                return false;
+            
+            field = value;
+            
+            handler?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+            return true;
+        }
+        
+        // returns true if we set property to a new value
+        public static bool SetField<T>(this INotifyPropertyChangedExt sender, ref T field, T value, bool compareRefOnly = false, [CallerMemberName] string propertyName = null)
+        {
+            if (!FieldCompare(field, value, compareRefOnly)) 
+                return false;
+            
+            field = value;
+            
+            sender.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        public static bool FieldCompare<T>(T field, T value, bool compareRefOnly = false)
+        {
             if (compareRefOnly)
             {
                 if (ReferenceEquals(field, value))
                     return false;
-            } 
+            }
             else if (EqualityComparer<T>.Default.Equals(field, value))
             {
                 return false;
             }
-            field = value;
-            
-            handler?.Invoke(sender, new PropertyChangedEventArgs(propertyName));
+
             return true;
         }
     }
