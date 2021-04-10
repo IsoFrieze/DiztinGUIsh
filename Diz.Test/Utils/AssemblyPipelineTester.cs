d232d2793f4f588b90afdae9fed9cd182409cd44
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-04-10 14:39:01 -0400

    add test harness for checking assembly output against embedded resources

---
+++ Diz.Test/Utils/AssemblyPipelineTester.cs
@@ -0,0 +1,25 @@
+﻿using Diz.Core.model;
+
+namespace Diz.Test.Utils
+{
+    public class AssemblyPipelineTester
+    {
+        public Data Source { get; init; }
+        public string ExpectedAsmOutput { get; init; }
+
+        public virtual void Test()
+        {
+            var outputAsmToTest = LogWriterHelper.ExportAssembly(Source);
+            LogWriterHelper.AssertAssemblyOutputEquals(ExpectedAsmOutput, outputAsmToTest);
+        }
+
+        public static AssemblyPipelineTester SetupFromResource(Data input, string expectedAsmOutputResource)
+        {
+            return new()
+            {
+                Source = input,
+                ExpectedAsmOutput = EmbeddedResourceDataAttribute.ReadResource(expectedAsmOutputResource)
+            };
+        }
+    }
+}
