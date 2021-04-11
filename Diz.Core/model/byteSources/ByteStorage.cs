27280e5959efc8b2d3595c325f4b124aa29fb09c
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-04-11 19:00:24 -0400

    implement (but not using yet) spare storage for ByteStorage

--- Diz.Core/model/byteSources/ByteStorage.cs
+++ Diz.Core/model/byteSources/ByteStorage.cs
@@ -1,23 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
+using System.Threading;
 using JetBrains.Annotations;
 
 namespace Diz.Core.model.byteSources
 {
     public abstract class ByteStorage : IEnumerable<ByteOffsetData>
     {
-        public ByteOffsetData this[int index]
-        {
-            get => GetByte(index);
-            set => SetByte(index, value);
-        }
+        public abstract ByteOffsetData this[int index] { get; set; }
 
         public abstract int Count { get; }
 
-        protected abstract void SetByte(int index, ByteOffsetData value);
-        protected abstract ByteOffsetData GetByte(int index);
-        
         protected ByteSource ParentContainer { get; }
 
         protected ByteStorage(ByteSource parent)
@@ -40,7 +36,6 @@ namespace Diz.Core.model.byteSources
 
         private void InitFromEmpty(int emptyCreateSize)
         {
-            Debug.Assert(ParentContainer != null);
             Debug.Assert(emptyCreateSize >= 0);
             
             InitEmptyContainer(emptyCreateSize);
@@ -51,7 +46,6 @@ namespace Diz.Core.model.byteSources
 
         protected void InitFrom(IReadOnlyCollection<ByteOffsetData> inBytes)
         {
-            Debug.Assert(ParentContainer != null);
             Debug.Assert(inBytes != null);
             
             InitEmptyContainer(inBytes.Count);
@@ -68,8 +62,6 @@ namespace Diz.Core.model.byteSources
 
         protected void OnPreAddByteAt(int newIndex, ByteOffsetData byteOffset)
         {
-            Debug.Assert(ParentContainer != null);
-            
             // cache these values
             byteOffset.Container = ParentContainer;
             byteOffset.ContainerOffset = newIndex; // this will be true after the Add() call below.
@@ -82,6 +74,47 @@ namespace Diz.Core.model.byteSources
         {
             return GetEnumerator();
         }
+        
+        protected void ImportBytes(IReadOnlyCollection<ByteOffsetData> inBytes)
+        {
+            Debug.Assert(inBytes != null);
+            foreach (var b in inBytes)
+            {
+                AddByte(b);
+            }
+        }
+
+        // iterate through a sparse ByteStorage class, if we encounter any gaps in the sequence,
+        // fill them in 
+        protected class GapFillingEnumerator : IEnumerator<ByteOffsetData>
+        {
+            public ByteStorage ByteStorage { get; protected set; }
+            public int Position { get; set; } = -1;
+
+            public GapFillingEnumerator(ByteStorage storage)
+            {
+                Debug.Assert(storage != null);
+                ByteStorage = storage;
+            }
+            public bool MoveNext()
+            {
+                Position++;
+                return Position < ByteStorage.Count;
+            }
+
+            public void Reset()
+            {
+                Position = -1;
+            }
+
+            ByteOffsetData IEnumerator<ByteOffsetData>.Current => ByteStorage[Position];
+            public object Current => ByteStorage[Position];
+            public void Dispose()
+            {
+                Position = -1;
+                ByteStorage = null;
+            }
+        }
     }
     
     // Simple version of byte storage that stores everything as an actual list
@@ -89,17 +122,17 @@ namespace Diz.Core.model.byteSources
     // address spaces (24bits of addressible bytes x HUGE data = slowwwww)
     public class ByteList : ByteStorage
     {
+        public override ByteOffsetData this[int index]
+        {
+            get => bytes[index];
+            set
+            {
+                OnPreAddByteAt(index, value);
+                bytes[index] = value;
+            }
+        }
+
         public override int Count => bytes?.Count ?? 0;
-
-        protected override void SetByte(int index, ByteOffsetData value)
-        {
-            bytes[index] = value;
-        }
-
-        protected override ByteOffsetData GetByte(int index)
-        {
-            return bytes[index];
-        }
 
         // only ever use AddByte() to add bytes here
         private List<ByteOffsetData> bytes = new();
@@ -117,14 +150,9 @@ namespace Diz.Core.model.byteSources
 
         protected override void FillEmptyContainerWithBytesFrom(IReadOnlyCollection<ByteOffsetData> inBytes)
         {
-            Debug.Assert(inBytes != null);
-
-            foreach (var b in inBytes)
-            {
-                AddByte(b);
-            }
+            ImportBytes(inBytes);
         }
-
+        
         protected override void FillEmptyContainerWithBlankBytes(int numEntries)
         {
             for (var i = 0; i < numEntries; ++i)
@@ -146,19 +174,117 @@ namespace Diz.Core.model.byteSources
         // NOTE: in this implementation, all bytes at all addresses always exist, so,
         // this will never return null or have gaps in the sequence.
         //
-        // other implementations may differ
+        // other implementations may differ.
         public override IEnumerator<ByteOffsetData> GetEnumerator()
         {
             return bytes.GetEnumerator();
         }
     }
 
-    /*public class SparseByteStorage : IByteStorage
+    public class SparseByteStorage : ByteStorage
     {
-        public IReadOnlyList<ByteOffsetData> Bytes { get; }
-        public void AddByte(ByteSource parent, ByteOffsetData byteOffset)
+        [UsedImplicitly] public SparseByteStorage(ByteSource parent) : base(parent) { }
+        [UsedImplicitly] public SparseByteStorage(ByteSource parent, IReadOnlyCollection<ByteOffsetData> inBytes) : base(parent, inBytes) { }
+        [UsedImplicitly] public SparseByteStorage(ByteSource parent, int emptyCreateSize) : base(parent, emptyCreateSize) { }
+
+        // keeps the keys sorted, which is what we want.
+        public SortedDictionary<int, ByteOffsetData> bytes;
+
+        private int GetLargestKey()
         {
-            throw new NotImplementedException();
+            if (bytes == null || bytes.Count == 0)
+                return -1;
+            
+            return bytes.Keys.Last();
         }
-    }*/
+
+        public override ByteOffsetData this[int index]
+        {
+            get => GetByte(index);
+            set => SetByte(index, value);
+        }
+
+        private void SetByte(int index, ByteOffsetData value)
+        {
+            ValidateIndex(index);
+            OnPreAddByteAt(index, value);
+
+            // will replace if it exists
+            bytes[index] = value;
+        }
+
+        // we need to maintain this. it's not the # of bytes we're storing,
+        // it's the max size of the sparse container. i.e. this will never change.
+        private int count = 0;
+        public override int Count => count;
+
+        private bool ValidIndex(int index) => index >= 0 && index < Count;
+
+        private void ValidateIndex(int index)
+        {
+            if (!ValidIndex(index))
+                throw new ArgumentOutOfRangeException($"Index {index} out of range in SparseByteStorage");   
+        }
+
+        protected ByteOffsetData GetByte(int index)
+        {
+            ValidateIndex(index);
+            return bytes.GetValueOrDefault(index);
+        }
+
+        protected override void InitEmptyContainer(int emptyCreateSize)
+        {
+            bytes = new SortedDictionary<int, ByteOffsetData>();
+            count = emptyCreateSize;
+        }
+
+        protected override void FillEmptyContainerWithBytesFrom(IReadOnlyCollection<ByteOffsetData> inBytes)
+        {
+            ImportBytes(inBytes);
+        }
+
+        protected override void FillEmptyContainerWithBlankBytes(int numEntries)
+        {
+            // for sparse version, this is mostly a NOP.
+            // we don't need to do anything here since the dictionary should have
+            // already been created.  in our implementation there's no such thing as "blank"
+            // bytes, we'll just return null if that address isn't in our dictionary.
+
+            count = numEntries;
+        }
+
+        public override void AddByte(ByteOffsetData byteOffset)
+        {
+            // going to be a little weird. this would normally be "append" however it's
+            // arbitrary where to do that with a dictionary. we wil interpret this as taking the highest
+            // index (key i.e. SNES or ROM offset) and adding one to that.
+
+            var largestKey = GetLargestKey();
+            var indexThisWillBeAddedTo = largestKey + 1; // go one higher than our biggest
+
+            ValidateIndex(indexThisWillBeAddedTo);
+            OnPreAddByteAt(indexThisWillBeAddedTo, byteOffset);
+            
+            bytes[indexThisWillBeAddedTo] = byteOffset;
+            count++;
+        }
+
+        // for our normal enumerator facing client code, use our special enumerator
+        // which will return sequential items but fill in the aps with Null.
+        //
+        // this is not the most efficient thing but makes the client code easier
+        // to write. if performance becomes an issue, use GetSparseEnumerator()
+        // which will just return the sections that have been populated.
+        public override IEnumerator<ByteOffsetData> GetEnumerator()
+        {
+            return new GapFillingEnumerator(this);
+        }
+
+        // note: indices are going to be ordered, BUT there can be gaps.
+        // caller should be prepared to handle this. 
+        public SortedDictionary<int, ByteOffsetData>.Enumerator GetSparseEnumerator()
+        {
+            return bytes.GetEnumerator();
+        }
+    }
 }
---
+++ Diz.Test/tests/ByteStorageTests.cs
@@ -0,0 +1,30 @@
+﻿using Diz.Core.model.byteSources;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Xunit;
+
+namespace Diz.Test.tests
+{
+    public static class ByteStorageTests
+    {
+        [Fact]
+        public static void TestSparseStorage()
+        {
+            ByteStorage byteStorage = new SparseByteStorage(null, 10);
+            byteStorage[1] = new ByteOffsetData {Byte = 0xE1};
+            byteStorage[7] = new ByteOffsetData {Byte = 0xE7};
+
+            var i = 0;
+            foreach (var b in byteStorage)
+            {
+                if (i == 1 || i == 7)
+                    Assert.True(b.Byte == 0xE0 + i);
+                else
+                    Assert.Null(b);
+
+                ++i;
+            }
+            
+            Assert.Equal(10, i);
+        } 
+    }
+}
