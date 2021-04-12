bc049dee043f69df98df1fa12bd53d235eae817f
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-04-12 16:49:40 -0400

    reworking XML serializer tests

--- Diz.Test/SerializerDictionaryTest.cs
+++ Diz.Test/SerializerDictionaryTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Xml;
 using Diz.Core.model;
 using Diz.Core.serialization.xml_serializer;
@@ -26,7 +27,7 @@ namespace Diz.Test
                 {1, new Comment{Text="Z test1"}},
                 {2, new Comment{Text="Z test3"}},
             };
-            public Dictionary<int, Label> Odw2 { get; } = new() {
+            /*public Dictionary<int, Label> Odw2 { get; } = new() {
                 {100, new Label {Comment = "c1", Name = "location1"}},
                 {200, new Label {Comment = "c2", Name = "location2"}},
             };
@@ -54,7 +55,7 @@ namespace Diz.Test
                     return ((Odw != null ? Odw.GetHashCode() : 0) * 397) ^ (Odw2 != null ? Odw2.GetHashCode() : 0);
                 }
             }
-            #endregion
+            #endregion*/
         }
 
         private static IConfigurationContainer GetSerializer()
@@ -62,34 +63,55 @@ namespace Diz.Test
             return XmlSerializerSupport.GetSerializer()
                 .EnableImplicitTyping(typeof(TestRoot));
         }
+        
+        private static Dictionary<int, Comment> GetSerializeData()
+        {
+            return new()
+            {
+                {1, new Comment{Text="Z test1"}},
+                {2, new Comment{Text="Z test2"}},
+            };
+        }
 
         [Theory]
         [EmbeddedResourceData("Diz.Test/Resources/serialize-dictionary-test.xml")]
         private void Serializer(string expectedXml)
         {
+            var expectedCleanedXml = SortaCleanupXml(expectedXml);
+            
             var serializer = GetSerializer().Create();
-            var toSerialize = new TestRoot();
+            var toSerialize = GetSerializeData();
 
+            /*var xmlStr = serializer.Serialize(
+                new XmlWriterSettings(),
+                toSerialize);*/
+            
             var xmlStr = serializer.Serialize(
                 new XmlWriterSettings(),
                 toSerialize);
 
             testOutputHelper.WriteLine(xmlStr);
 
-            Assert.Equal(expectedXml, xmlStr);
+            Assert.Equal(expectedCleanedXml, xmlStr);
         }
 
-        [Fact]
-        private void DeSerialize()
+        private static string SortaCleanupXml(string expectedXml)
         {
-            var serializer = GetSerializer().Create();
-            var restoredRoot = serializer.Deserialize<TestRoot>(XmlShouldBe);
-
-            var expected = new TestRoot();
-
-            Assert.Equal(expected, restoredRoot);
+            return expectedXml.Replace("\r\n", "");
         }
 
-        const string XmlShouldBe = "<?xml version=\"1.0\" encoding=\"utf-8\"?><SerializerDictionaryTest-TestRoot xmlns:ns1=\"clr-namespace:IX.Observable;assembly=IX.Observable\" xmlns:sys=\"https://extendedxmlserializer.github.io/system\" xmlns:exs=\"https://extendedxmlserializer.github.io/v2\" xmlns:ns2=\"clr-namespace:Diz.Core.model;assembly=Diz.Core\"><Odw AutomaticallyCaptureSubItems=\"false\" HistoryLevels=\"50\"><sys:Item Key=\"1\"><Value Text=\"Z test1\" /></sys:Item><sys:Item Key=\"2\"><Value Text=\"Z test3\" /></sys:Item></Odw><Odw2 AutomaticallyCaptureSubItems=\"false\" HistoryLevels=\"50\"><sys:Item Key=\"100\"><Value Name=\"location1\" Comment=\"c1\" /></sys:Item><sys:Item Key=\"200\"><Value Name=\"location2\" Comment=\"c2\" /></sys:Item></Odw2></SerializerDictionaryTest-TestRoot>";
+        [Theory]
+        [EmbeddedResourceData("Diz.Test/Resources/serialize-dictionary-test.xml")]
+        private void DeSerialize(string inputXml)
+        {
+            var inputCleanedXml = SortaCleanupXml(inputXml);
+            
+            var expectedObj = GetSerializeData();
+
+            var serializer = GetSerializer().Create();
+            var actuallyDeserialized = serializer.Deserialize<Dictionary<int, Comment>>(inputCleanedXml);
+
+            Assert.Equal(expectedObj, actuallyDeserialized);
+        }
     }
 }
