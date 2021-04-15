f1e6dada99731985f002effe185288855db8808a
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-04-15 08:59:22 -0400

    add parent-aware collection, will be base class for a bunch of new stuff - i need to refactor more of the classes to use these

---
+++ Diz.Core/util/ParentAwareCollection.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Diz.Core.util
+{
+    public interface IParentAware<in TParent>
+    {
+        public void OnParentChanged(TParent parent);
+    }
+
+    public class ParentAwareCollection<TParent, TItem> : ParentAwareCollectionBase<TParent, TItem>
+        where TParent : class
+        where TItem : IParentAware<TParent>
+    {
+        public void RemoveAll(Predicate<TItem> match)
+        {
+            if (match == null)
+                throw new ArgumentNullException(nameof(match));
+
+            this.Where(item => match(item)).ToList().ForEach(item => Remove(item));
+        }
+        
+        public void AddRange(IEnumerable<TItem> newItems)
+        {
+            foreach (var newItem in newItems)
+            {
+                Add(newItem);
+            }
+        }
+    }
+
+    public class ParentAwareCollectionBase<TParent, TItem> : Collection<TItem>
+        where TParent : class
+        where TItem : IParentAware<TParent>
+    {
+        private TParent parent;
+
+        [PublicAPI]
+        public TParent Parent
+        {
+            get => parent;
+            set
+            {
+                parent = value;
+                UpdateAllItemParent(parent);
+            }
+        }
+
+        private static void SetItemParent(TItem item, TParent newParent)
+        {
+            item?.OnParentChanged(newParent);
+        }
+
+        private void UpdateAllItemParent(TParent newParent)
+        {
+            foreach (var item in this)
+            {
+                SetItemParent(item, newParent);
+            }
+        }
+
+        protected override void ClearItems()
+        {
+            UpdateAllItemParent(null);
+            base.ClearItems();
+        }
+
+        protected override void InsertItem(int index, TItem item)
+        {
+            base.InsertItem(index, item);
+            item?.OnParentChanged(Parent);
+        }
+
+        protected override void RemoveItem(int index)
+        {
+            var item = Items[index];
+            item?.OnParentChanged(null);
+            base.RemoveItem(index);
+        }
+
+        protected override void SetItem(int index, TItem item)
+        {
+            base.SetItem(index, item);
+            item?.OnParentChanged(Parent);
+        }
+    }
+}
