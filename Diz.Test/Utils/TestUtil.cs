9ac79fefcab6e1828db4fc27c106f6c1825317a7
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-05-09 01:42:56 -0400

    backport test runner utils from 3.0dev branch

---
+++ Diz.Test/Utils/TestUtil.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace Diz.Test.Utils
+{
+    public static class TheoryDataGenerator
+    {
+        public static TheoryData<T> CreateTheoryData<T>(this IEnumerable<Func<T>> data)
+        {
+            return data
+                .Select(fn => fn())
+                .Aggregate(new TheoryData<T>(), (theoryData, item) =>
+                {
+                    theoryData.Add(item);
+                    return theoryData;
+                });
+        }
+    }
+    
+    public static class TestUtil
+    {
+        public static void AssertCollectionEqual<T>(IReadOnlyList<T> expected, IReadOnlyList<T> actual)
+        {
+            // do some weirdness here to better display the differences in the output window.
+            
+            var largestListCount = Math.Max(expected.Count, actual.Count);
+            for (var i = 0; i < largestListCount; ++i)
+            {
+                // if this gets hit, lengths of lists are different
+                Assert.True(i < actual.Count);
+                Assert.True(i < expected.Count);
+
+                var expectedItem = expected[i];
+                var actualItem = actual[i];
+                
+                Assert.Equal(expectedItem, actualItem);
+            }
+            
+            Assert.Equal(expected.Count, actual.Count);
+
+            Assert.True(expected.SequenceEqual(actual));
+        }
+    }
+}
---
+++ Diz.Test/Utils/XmlTestUtils.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Diz.Core.serialization.xml_serializer;
+using ExtendedXmlSerializer;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Diz.Test.Utils
+{
+    public class XmlTestUtils
+    {
+        public ITestOutputHelper Output { get; set; }
+        
+        public void RunFullCycle<T>(Func<T> createFn, out T expectedCopy, out T deserializedObj)
+        {
+            RunFullCycleObj(() => createFn(), out var expectedObjCopy, out var deserializedObjCopy);
+
+            expectedCopy = (T)expectedObjCopy;
+            deserializedObj = (T) deserializedObjCopy;
+        }
+
+        public void RunFullCycleObj(Func<object> createFn, out object expectedCopy, out object deserializedObj)
+        {
+            var objToCycle = createFn();
+            expectedCopy = createFn();
+            
+            deserializedObj = XmlFullCycle(objToCycle);
+        }
+        
+        public T XmlFullCycle<T>(T objToCycle)
+        {
+            var xmlToCycle = XmlSerializerSupport.GetSerializer().Create().Serialize(objToCycle);
+            Output?.WriteLine(xmlToCycle);
+            var deserialized = XmlSerializerSupport.GetSerializer().Create().Deserialize<T>(xmlToCycle);
+            return deserialized;
+        }
+        
+        public void RunFullCycle(Func<object> createFn)
+        {
+            RunFullCycle(createFn, out var unchanged, out var cycled);
+            Assert.Equal(unchanged, cycled);
+        }
+    }
+}
