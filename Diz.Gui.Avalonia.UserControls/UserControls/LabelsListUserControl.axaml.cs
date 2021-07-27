76e95d185549241e92cd217343e0d20d663352bf
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-07-27 01:23:02 -0400

    so close to working well

--- Diz.Gui.Avalonia.UserControls/UserControls/LabelsListUserControl.axaml.cs
+++ Diz.Gui.Avalonia.UserControls/UserControls/LabelsListUserControl.axaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reactive.Disposables;
+using System.Reactive.Linq;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Avalonia.ReactiveUI;
@@ -11,83 +12,38 @@ namespace Diz.Gui.Avalonia.UserControls.UserControls
     {
         public DataGrid LabelGrid => this.FindControl<DataGrid>("LabelGrid");
 
+        public TextBox TxtOffsetSearch => this.FindControl<TextBox>("TxtOffsetSearch");
+
         public LabelsListUserControl()
         {
             ViewModel = new LabelsViewModel();
 
             this.WhenActivated(disposables =>
             {
-                // prob how we should do it with commands to edit
                 this.OneWayBind(ViewModel,
                     viewmodel => viewmodel.Labels,
                     view => view.LabelGrid.Items
                 ).DisposeWith(disposables);
-                //
-                // two way binding (try this?)
+
                 // this.Bind(ViewModel,
-                //     viewmodel => viewmodel.ByteEntries,
+                //     viewmodel => viewmodel.Labels,
                 //     view => view.LabelGrid.Items
                 // ).DisposeWith(disposables);
 
                 this.WhenAnyValue(x => x.LabelGrid.SelectedItem)
                     .BindTo(this, x => x.ViewModel.SelectedItem);
 
+                this.WhenAnyValue(x => x.TxtOffsetSearch.Text)
+                    .BindTo(this, x => x.ViewModel.OffsetFilter);
+
                 this.Bind(ViewModel,
                     vm => vm.SelectedItem,
                     v => v.LabelGrid.SelectedItem
                 );
-
-                // LabelGrid.LoadingRow += LabelGridOnLoadingRow;
-
-                // var x = new Style(
-                //     x => x.OfType<DataGridCell>()
-                //         .PropertyEquals(DataGridCell.NameProperty, true));
-
-                //
-                // this.
-                // // Observable.FromEventPattern<DataGridCellEditEndedEventArgs>()
-                //
-                // LabelGrid.CellEditEnded
-
-                // this.BindCommand(ViewModel, vm=>vm.)
-
-                // this.BindCommand(
-                //     ViewModel,
-                //     vm => vm.SetSelectedItem,
-                //     v => v.LabelGrid,
-                //     nameof(LabelGrid.CellEditEnded));
-
-                // this.BindCommand(
-                //     ViewModel,
-                //     vm => vm.SetSelectedItem,
-                //     v => v.LabelGrid.SelectedItem,
-                //     nameof(LabelGrid.CellEditEnded));
-
-//                this.BindCommand(ViewModel,
-                //                  vm => vm.ByteEntries,
-                //                view=>view.LabelGrid.Items[0]
             });
 
             InitializeComponent();
         }
-
-        // private void LabelGridOnLoadingRow(object sender, DataGridRowEventArgs e)
-        // {
-        //     if (e.Row.DataContext is not LabelsViewModel byteEntryDetailsViewModel)
-        //         return;
-        //
-        //     // set colors/etc
-        //     // better ways to do this. HACK
-        //
-        //     // var x = new Setter();
-        //     // x.
-        //
-        //     // dataObject.ByteEntry.
-        //     // e.Row.Background = Brushes.Red;
-        //
-        //
-        //     // e.Row.Background = new SolidColorBrush(Color)
-        // }
 
         private void InitializeComponent()
         {
--- Diz.Gui.ViewModels/ViewModels/LabelsViewModel.cs
+++ Diz.Gui.ViewModels/ViewModels/LabelsViewModel.cs
@@ -1,32 +1,80 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Diz.Core.model;
+using DynamicData;
+using DynamicData.Binding;
 using ReactiveUI;
 
 namespace Diz.Gui.ViewModels.ViewModels
 {
-    public class LabelsViewModel : ViewModel
+    public class LabelViewModel : ViewModel
     {
-        public ObservableCollection<Label> Labels
+        private Label label;
+
+        public Label Label
         {
-            get => labels;
-            set => this.RaiseAndSetIfChanged(ref labels, value);
+            get => label;
+            set => this.RaiseAndSetIfChanged(ref label, value);
         }
 
-        private Label selectedItem;
-        private ObservableCollection<Label> labels;
+        public LabelViewModel()
+        {
+            // this.WhenActivated(disposableRegistration =>
+            // {
+            //     label.WhenAnyValue(x => x.Comment)
+            //         .Subscribe(x =>
+            //             Console.WriteLine($"changed! {x}")
+            //         );
+            // });
+        }
+    }
 
-        public Label SelectedItem
+    public class LabelsViewModel : ViewModel
+    {
+        // public ObservableCollection<Label> Labels
+        // {
+        //     get => labels;
+        //     set => this.RaiseAndSetIfChanged(ref labels, value);
+        // }
+
+        private readonly ObservableAsPropertyHelper<IEnumerable<LabelViewModel>> _labels;
+        public IEnumerable<LabelViewModel> Labels => _labels.Value;
+
+        private LabelViewModel selectedItem;
+
+        // private ObservableCollection<Label> labels;
+        private string offsetFilter;
+
+        public LabelViewModel SelectedItem
         {
             get => selectedItem;
             set => this.RaiseAndSetIfChanged(ref selectedItem, value);
         }
 
+        public string OffsetFilter
+        {
+            get => offsetFilter;
+            set => this.RaiseAndSetIfChanged(ref offsetFilter, value);
+        }
+
         public LabelsViewModel()
         {
             // temp hack
-            Labels = new ObservableCollection<Label>(GetSampleLabels() ?? new List<Label>());
+            // Labels = new ObservableCollection<Label>(GetSampleLabels() ?? new List<Label>());
+
+            // Labels.ToObservableChangeSet();
+
+            // var sourceCache = new SourceCache<Label, int>(x => x.Offset);
+            // var outObservableCollection = new ObservableCollectionExtended<Label>();
+            // sourceCache
+            //     .Connect()
+            //     .Bind(outObservableCollection);
 
             // byteEntries = this
             //     .WhenAnyValue(x => x.StartingOffset)
@@ -38,14 +86,53 @@ namespace Diz.Gui.ViewModels.ViewModels
             //     .ObserveOn(RxApp.MainThreadScheduler)
             //     .ToProperty(this, x => x.ByteEntries);
 
+            _labels = this.WhenAnyValue(x => x.OffsetFilter)
+                .Throttle(TimeSpan.FromMilliseconds(100))
+                .Select(term => term?.Trim() ?? "")
+                .DistinctUntilChanged()
+                .SelectMany(SearchLabels)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .ToProperty(this, x=>x.Labels);
+
             this.WhenActivated(
                 (CompositeDisposable disposables) =>
                 {
-                    // ReactiveCommand.Create<DataGridCellEditEndedEventArgs>(CellEdited);                    
+                    // ReactiveCommand.Create<DataGridCellEditEndedEventArgs>(CellEdited);
+                    // this
+                    //     .WhenAnyValue(x => x.Labels)
+                    //     .Subscribe(x);
                 });
         }
 
-        private List<Label> GetSampleLabels()
+        // private IEnumerable<LabelViewModel> SearchLabels(string searchTerm)
+        private static async Task<IEnumerable<LabelViewModel>> SearchLabels(string searchTerm, CancellationToken token)
+        {
+            return await Task.Run(() =>
+            {
+                var sourceData = FakeModel.SourceLabels.Value;
+                return sourceData.Items
+                    .Where(x=>
+                        SearchLabelView(x, searchTerm)
+                    );
+            }, token);
+        }
+
+        private static bool SearchLabelView(LabelViewModel x, string searchTerm) =>
+            string.IsNullOrEmpty(searchTerm) || 
+            x.Label.Offset.ToString().Contains(searchTerm);
+    }
+
+    public static class FakeModel
+    {
+        public static Lazy<SourceCache<LabelViewModel, int>> SourceLabels
+            = new(valueFactory: () =>
+            {
+                var sourceCache = new SourceCache<LabelViewModel, int>(x => x.Label.Offset);
+                sourceCache.AddOrUpdate(CreateSampleLabels());
+                return sourceCache;
+            });
+
+        private static IEnumerable<LabelViewModel> CreateSampleLabels()
         {
             // var loader = Service.Container.GetInstance<ISampleProjectLoader>("SampleProjectLoader");
 
@@ -77,25 +164,28 @@ namespace Diz.Gui.ViewModels.ViewModels
             //     }
             // });
 
-            return new()
-            {
-                new Label
+            return new List<Label>
                 {
-                    Comment = "test2",
-                    Name = "name2",
-                    Offset = 2,
-                },
-                new Label
-                {
-                    Comment = "test1",
-                    Name = "name1", Offset = 1,
-                },
-                new Label
-                {
-                    Comment = "test3",
-                    Name = "name3", Offset = 3,
+                    new()
+                    {
+                        Comment = "test2",
+                        Name = "name2",
+                        Offset = 2,
+                    },
+                    new()
+                    {
+                        Comment = "test1",
+                        Name = "name1", Offset = 1,
+                    },
+                    new()
+                    {
+                        Comment = "test3",
+                        Name = "name3", Offset = 3,
+                    }
                 }
-            };
+                .Select(x=>
+                    new LabelViewModel {Label = x}
+                ).ToList();
         }
     }
 }
