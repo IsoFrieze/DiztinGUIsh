32ad56ab91a8262e20cad6fd32bf02da0195fc02
FS <2019363+blackerking@users.noreply.github.com>
2022-02-13 13:09:31 +0100

    fixing mistake

--- DiztinGUIsh/window/dialog/GotoDialog.cs
+++ DiztinGUIsh/window/dialog/GotoDialog.cs
@@ -1,181 +1,171 @@
+using System;
+using System.Globalization;
+using System.Windows.Forms;
+using Diz.Core.model;
+using Diz.Core.util;
+
 namespace DiztinGUIsh.window.dialog
 {
-    partial class GotoDialog
+    public partial class GotoDialog : Form
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
-
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
+        public Data Data { get; set; }
+        public GotoDialog(int offset, Data data)
         {
-            if (disposing && (components != null))
+            InitializeComponent();
+            Data = data;
+            textROM.Text = Util.NumberToBaseString(Data.ConvertPCtoSnes(offset), Util.NumberBase.Hexadecimal, 6);
+            textPC.Text = Util.NumberToBaseString(offset, Util.NumberBase.Hexadecimal, 0);
+        }
+
+        private void GotoDialog_Load(object sender, EventArgs e)
+        {
+            textROM.SelectAll();
+            UpdateUi();
+        }
+
+        private int ParseOffset(string text)
+        {
+            NumberStyles style = radioDec.Checked ? NumberStyles.Number : NumberStyles.HexNumber;
+            if (int.TryParse(text, style, null, out int offset)) return offset;
+            return -1;
+        }
+
+        public int GetPcOffset()
+        {
+            return ParseOffset(textPC.Text);
+        }
+
+        private void Go()
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private bool updatingText;
+
+        private bool UpdateTextChanged(string txtChanged, Action<string, int, Util.NumberBase> onSuccess)
+        {
+            bool result = false;
+            if (!updatingText)
             {
-                components.Dispose();
+                updatingText = true;
+
+                var style = radioDec.Checked ? NumberStyles.Number : NumberStyles.HexNumber;
+                var noBase = radioDec.Checked ? Util.NumberBase.Decimal : Util.NumberBase.Hexadecimal;
+
+                if (ByteUtil.StripFormattedAddress(ref txtChanged, style, out var address) && address >= 0)
+                {
+                    onSuccess(txtChanged, address, noBase);
+                    result = true;
+                }
+                updatingText = false;
             }
-            base.Dispose(disposing);
+
+            return result;
         }
 
-        #region Windows Form Designer generated code
+        // For both textbox TextChanged events:
+        // precondition: unvalidated input in textbox
+        // postcondtion: valid text is in both textboxes, or, button is greyed out and error message displayed.
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+        private void UpdateUi()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textROM = new System.Windows.Forms.TextBox();
-            this.textPC = new System.Windows.Forms.TextBox();
-            this.radioHex = new System.Windows.Forms.RadioButton();
-            this.radioDec = new System.Windows.Forms.RadioButton();
-            this.cancel = new System.Windows.Forms.Button();
-            this.go = new System.Windows.Forms.Button();
-            this.lblError = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(25, 15);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "SNES Address:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 41);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "PC Offset:";
-            // 
-            // textROM
-            // 
-            this.textROM.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textROM.Location = new System.Drawing.Point(103, 12);
-            this.textROM.MaxLength = 8;
-            this.textROM.Name = "textROM";
-            this.textROM.Size = new System.Drawing.Size(65, 20);
-            this.textROM.TabIndex = 1;
-            this.textROM.Text = "000000";
-            this.textROM.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.textROM.TextChanged += new System.EventHandler(this.textROM_TextChanged);
-            this.textROM.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textROM_KeyDown);
-            // 
-            // textPC
-            // 
-            this.textPC.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textPC.Location = new System.Drawing.Point(103, 38);
-            this.textPC.MaxLength = 8;
-            this.textPC.Name = "textPC";
-            this.textPC.Size = new System.Drawing.Size(65, 20);
-            this.textPC.TabIndex = 3;
-            this.textPC.Text = "00000";
-            this.textPC.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.textPC.TextChanged += new System.EventHandler(this.textPC_TextChanged);
-            this.textPC.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textPC_KeyDown);
-            // 
-            // radioHex
-            // 
-            this.radioHex.AutoSize = true;
-            this.radioHex.Checked = true;
-            this.radioHex.Location = new System.Drawing.Point(43, 70);
-            this.radioHex.Name = "radioHex";
-            this.radioHex.Size = new System.Drawing.Size(44, 17);
-            this.radioHex.TabIndex = 4;
-            this.radioHex.TabStop = true;
-            this.radioHex.Text = "Hex";
-            this.radioHex.UseVisualStyleBackColor = true;
-            this.radioHex.CheckedChanged += new System.EventHandler(this.radioHex_CheckedChanged);
-            // 
-            // radioDec
-            // 
-            this.radioDec.AutoSize = true;
-            this.radioDec.Location = new System.Drawing.Point(93, 70);
-            this.radioDec.Name = "radioDec";
-            this.radioDec.Size = new System.Drawing.Size(45, 17);
-            this.radioDec.TabIndex = 5;
-            this.radioDec.Text = "Dec";
-            this.radioDec.UseVisualStyleBackColor = true;
-            // 
-            // cancel
-            // 
-            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.cancel.Location = new System.Drawing.Point(12, 121);
-            this.cancel.Name = "cancel";
-            this.cancel.Size = new System.Drawing.Size(75, 23);
-            this.cancel.TabIndex = 7;
-            this.cancel.TabStop = false;
-            this.cancel.Text = "Cancel";
-            this.cancel.UseVisualStyleBackColor = true;
-            this.cancel.Click += new System.EventHandler(this.cancel_Click);
-            // 
-            // go
-            // 
-            this.go.Location = new System.Drawing.Point(93, 121);
-            this.go.Name = "go";
-            this.go.Size = new System.Drawing.Size(75, 23);
-            this.go.TabIndex = 6;
-            this.go.Text = "Go";
-            this.go.UseVisualStyleBackColor = true;
-            this.go.Click += new System.EventHandler(this.go_Click);
-            // 
-            // lblError
-            // 
-            this.lblError.AutoSize = true;
-            this.lblError.ForeColor = System.Drawing.Color.Red;
-            this.lblError.Location = new System.Drawing.Point(12, 102);
-            this.lblError.Name = "lblError";
-            this.lblError.Size = new System.Drawing.Size(39, 13);
-            this.lblError.TabIndex = 8;
-            this.lblError.Text = "lblError";
-            // 
-            // GotoDialog
-            // 
-            this.AcceptButton = this.go;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.CancelButton = this.cancel;
-            this.ClientSize = new System.Drawing.Size(180, 153);
-            this.Controls.Add(this.lblError);
-            this.Controls.Add(this.go);
-            this.Controls.Add(this.cancel);
-            this.Controls.Add(this.radioDec);
-            this.Controls.Add(this.radioHex);
-            this.Controls.Add(this.textPC);
-            this.Controls.Add(this.textROM);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "GotoDialog";
-            this.ShowIcon = false;
-            this.ShowInTaskbar = false;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-            this.Text = "Goto";
-            this.Load += new System.EventHandler(this.GotoDialog_Load);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            var valid = true;
+            lblError.Text = "";
 
+            if (!IsPcOffsetValid())
+            {
+                lblError.Text = "Invalid ROM File Offset";
+                valid = false;
+            }
+
+            if (!IsRomAddressValid())
+            {
+                lblError.Text = "Invalid SNES Address";
+                valid = false;
+            }
+
+            go.Enabled = valid;
         }
 
-        #endregion
+        private bool IsValidPcAddress(int pc)
+        {
+            return pc >= 0 && pc < Data.GetRomSize();
+        }
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textROM;
-        private System.Windows.Forms.TextBox textPC;
-        private System.Windows.Forms.RadioButton radioHex;
-        private System.Windows.Forms.RadioButton radioDec;
-        private System.Windows.Forms.Button cancel;
-        private System.Windows.Forms.Button go;
-        private System.Windows.Forms.Label lblError;
+        private bool IsPcOffsetValid()
+        {
+            var offset = GetPcOffset();
+            return IsValidPcAddress(offset);
+        }
+
+        private bool IsRomAddressValid()
+        {
+            var address = ParseOffset(textROM.Text);
+            if (address < 0)
+                return false;
+
+            return IsValidPcAddress(Data.ConvertSnesToPc(address));
+        }
+
+        private void textROM_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTextChanged(textROM.Text,(finaltext, address, noBase) =>
+            {
+                int pc = Data.ConvertSnesToPc(address);
+
+                textROM.Text = finaltext;
+                textPC.Text = Util.NumberToBaseString(pc, noBase, 0);
+            });
+
+            UpdateUi();
+        }
+
+        private void textPC_TextChanged(object sender, EventArgs e)
+        {
+            UpdateTextChanged(textPC.Text, (finaltext, offset, noBase) =>
+            {
+                int addr = Data.ConvertPCtoSnes(offset);
+
+                textPC.Text = finaltext;
+                textROM.Text = Util.NumberToBaseString(addr, noBase, 6);
+            });
+
+            UpdateUi();
+        }
+
+        private void go_Click(object sender, EventArgs e)
+        {
+            Go();
+        }
+
+        private void cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textROM_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && textROM.Text.Length > 0) Go();
+        }
+
+        private void textPC_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && textPC.Text.Length > 0) Go();
+        }
+
+        private void radioHex_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioHex.Checked) {
+                if (int.TryParse(textPC.Text, out var result))
+                {
+                    textPC.Text = Util.NumberToBaseString(result, Util.NumberBase.Hexadecimal, 0);
+                }
+            } else {
+                if (int.TryParse(textPC.Text, NumberStyles.HexNumber, null, out var result))
+                {
+                    textPC.Text = result.ToString();
+                }
+            }
+        }
     }
 }
