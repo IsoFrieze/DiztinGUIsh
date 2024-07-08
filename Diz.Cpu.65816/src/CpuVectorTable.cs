c981d3387852114a48c6abe823c2873205342ff7
Dominic Cerquetti <binary1230@users.noreply.github.com>
2024-07-07 20:46:26 -0400

    CPU vector table: add notes about how we're probably doing it wrong - label vector tables too - see https://github.com/IsoFrieze/DiztinGUIsh/issues/74 for some info maybe

--- Diz.Cpu.65816/src/CpuVectorTable.cs
+++ Diz.Cpu.65816/src/CpuVectorTable.cs
@@ -7,11 +7,72 @@ public static class CpuVectorTable
     public const int VectorTableBaseOffset = 15;
     
     public record VectorTableEntry(string Name, int SettingsOffset);
-    public record VectorTable(string Name, int SettingsOffset, VectorTableEntry[] Entries);
+    public record VectorTable(int SettingsOffset, VectorTableEntry[] Entries);
+    
+    /*
+     * see:
+     *   https://github.com/IsoFrieze/DiztinGUIsh/issues/74
+     *   
+     * TODO, pretty sure vector table below is generating incorrect output.
+     *    it SHOULD be made to match this (examples from some random ROM):
+     
+                       // values if lowrom                                 
+Native_unused_vector1:
+                       dw $0000                             ;00FFE0|        |000000; unused by SNES
+
+Native_unused_vector2:
+                       dw $0000                             ;00FFE2|        |000000; unused by SNES
+                                                            ;      |        |      ;
+           Native_COP:
+                       dw fn_NOP_rti                        ;00FFE4|        |008138; Fzero: NO-OP
+                                                            ;      |        |      ;
+           Native_BRK:
+                       dw fn_NOP_rti                        ;00FFE6|        |008138; Fzero: NO-OP
+                                                            ;      |        |      ;
+  Native_ABORT_unused:
+                       dw fn_NOP_rti                        ;00FFE8|        |008138; unused by SNES
+                                                            ;      |        |      ;
+           Native_NMI:
+                       dw fn_native_NMI_routine             ;00FFEA|        |0080D9;
+                                                            ;      |        |      ;
+Native_unused_vector3:
+                       dw PTR16_00FFFF                      ;00FFEC|        |00FFFF; unused by SNES
+                                                            ;      |        |      ;
+           Native_IRQ:
+                       dw CODE_008601                       ;00FFEE|        |008601;
+                                                            ;      |        |      ;
+Emulation_unused_vector1:
+                       dw $0000                             ;00FFF0|        |000000; unused by SNES
+                                                            ;      |        |      ;
+Emulation_unused_vector2:
+                       dw $0000                             ;00FFF2|        |000000; unused by SNES
+                                                            ;      |        |      ;
+        Emulation_COP:
+                       dw fn_NOP_rti                        ;00FFF4|        |008138; Fzero: NO-OP
+                                                            ;      |        |      ;
+Emulation_unused_vector3:
+                       dw PTR16_00FFFF                      ;00FFF6|        |00FFFF; unused by SNES
+                                                            ;      |        |      ;
+      Emulation_ABORT:
+                       dw fn_NOP_rti                        ;00FFF8|        |008138; unused by SNES
+                                                            ;      |        |      ;
+        Emulation_NMI:
+                       dw fn_NOP_rti                        ;00FFFA|        |008138; Fzero: NO-OP
+                                                            ;      |        |      ;
+      Emulation_RESET:
+                       dw cpu_startup                       ;00FFFC|        |008000; entrypoint of entire game
+                                                            ;      |        |      ;
+        Emulation_IRQ:
+                       dw fn_NOP_rti                        ;00FFFE|        |008138; Fzero: NO-OP
+
+     
+     */
 
     public static readonly VectorTable[] VectorTables = 
     {
-        new("Native", VectorTableBaseOffset, new []
+        // https://ersanio.gitbook.io/assembly-for-the-snes/deep-dives/vector
+        // TODO: strong suspect some of this is wrong. see above.
+        new( VectorTableBaseOffset, new []
         {
             new VectorTableEntry(SnesVectorNames.Native_COP, 0x00),
             new VectorTableEntry(SnesVectorNames.Native_BRK, 0x02),
@@ -20,7 +81,7 @@ public static class CpuVectorTable
             new VectorTableEntry(SnesVectorNames.Native_RESET, 0x08),
             new VectorTableEntry(SnesVectorNames.Native_IRQ, 0x10)
         }),
-        new("Emulation", VectorTableBaseOffset + 0x12, new [] // prob this is wrong, 0x12?
+        new(VectorTableBaseOffset + 0x12, new [] // prob this is wrong, 0x12?
         {
             new VectorTableEntry(SnesVectorNames.Emulation_COP, 0x00),
             new VectorTableEntry(SnesVectorNames.Emulation_Unknown, 0x02),
