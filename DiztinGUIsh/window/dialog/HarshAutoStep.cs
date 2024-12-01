25533157167a9843e53ffa6da51b252d3abd4bfd
Dominic Cerquetti <binary1230@users.noreply.github.com>
2024-12-01 13:21:18 -0500

    fixup more namespaces

--- DiztinGUIsh/window/dialog/HarshAutoStep.cs
+++ DiztinGUIsh/window/dialog/HarshAutoStep.cs
@@ -5,114 +5,113 @@ using System.Windows.Forms;
 using Diz.Core.model.snes;
 using Diz.Core.util;
 
-namespace DiztinGUIsh
+namespace DiztinGUIsh.window.dialog;
+
+public partial class HarshAutoStep : Form
 {
-    public partial class HarshAutoStep : Form
-    {
-        public int StartRomOffset { get; private set; }
-        public int EndRomOffset { get; private set; }
-        public int Count { get; private set; }
+    public int StartRomOffset { get; private set; }
+    public int EndRomOffset { get; private set; }
+    public int Count { get; private set; }
         
-        private readonly Data data;
-        private bool updatingText;
+    private readonly Data data;
+    private bool updatingText;
 
-        public HarshAutoStep(int offset, Data data)
+    public HarshAutoStep(int offset, Data data)
+    {
+        Debug.Assert(data != null);
+        this.data = data;
+
+        InitializeComponent();
+
+        StartRomOffset = offset;
+        var rest = data.GetRomSize() - StartRomOffset;
+        Count = rest < 0x100 ? rest : 0x100;
+        EndRomOffset = StartRomOffset + Count;
+
+        UpdateText(null);
+    }
+
+    private void UpdateText(TextBox selected)
+    {
+        var noBase = radioDec.Checked ? Util.NumberBase.Decimal : Util.NumberBase.Hexadecimal;
+        var digits = noBase == Util.NumberBase.Hexadecimal && radioSNES.Checked ? 6 : 0;
+
+        if (StartRomOffset < 0) StartRomOffset = 0;
+        if (EndRomOffset >= data.GetRomSize()) EndRomOffset = data.GetRomSize() - 1;
+        Count = EndRomOffset - StartRomOffset;
+        if (Count < 0) Count = 0;
+
+        updatingText = true;
+        if (selected != textStart) textStart.Text = Util.NumberToBaseString(radioSNES.Checked ? data.ConvertPCtoSnes(StartRomOffset) : StartRomOffset, noBase, digits);
+        if (selected != textEnd) textEnd.Text = Util.NumberToBaseString(radioSNES.Checked ? data.ConvertPCtoSnes(EndRomOffset) : EndRomOffset, noBase, digits);
+        if (selected != textCount) textCount.Text = Util.NumberToBaseString(Count, noBase, 0);
+        updatingText = false;
+    }
+
+    private void radioHex_CheckedChanged(object sender, EventArgs e)
+    {
+        UpdateText(null);
+    }
+
+    private void radioROM_CheckedChanged(object sender, EventArgs e)
+    {
+        UpdateText(null);
+    }
+
+    private void textCount_TextChanged(object sender, EventArgs e)
+    {
+        OnTextChanged(textCount, value =>
         {
-            Debug.Assert(data != null);
-            this.data = data;
-
-            InitializeComponent();
-
-            StartRomOffset = offset;
-            var rest = data.GetRomSize() - StartRomOffset;
-            Count = rest < 0x100 ? rest : 0x100;
+            Count = value;
             EndRomOffset = StartRomOffset + Count;
+        });
+    }
 
-            UpdateText(null);
-        }
-
-        private void UpdateText(TextBox selected)
+    private void textEnd_TextChanged(object sender, EventArgs e)
+    {
+        OnTextChanged(textEnd, value =>
         {
-            var noBase = radioDec.Checked ? Util.NumberBase.Decimal : Util.NumberBase.Hexadecimal;
-            var digits = noBase == Util.NumberBase.Hexadecimal && radioSNES.Checked ? 6 : 0;
+            if (radioSNES.Checked)
+                value = data.ConvertSnesToPc(value);
 
-            if (StartRomOffset < 0) StartRomOffset = 0;
-            if (EndRomOffset >= data.GetRomSize()) EndRomOffset = data.GetRomSize() - 1;
+            EndRomOffset = value;
             Count = EndRomOffset - StartRomOffset;
-            if (Count < 0) Count = 0;
+        });
+    }
 
-            updatingText = true;
-            if (selected != textStart) textStart.Text = Util.NumberToBaseString(radioSNES.Checked ? data.ConvertPCtoSnes(StartRomOffset) : StartRomOffset, noBase, digits);
-            if (selected != textEnd) textEnd.Text = Util.NumberToBaseString(radioSNES.Checked ? data.ConvertPCtoSnes(EndRomOffset) : EndRomOffset, noBase, digits);
-            if (selected != textCount) textCount.Text = Util.NumberToBaseString(Count, noBase, 0);
-            updatingText = false;
-        }
-
-        private void radioHex_CheckedChanged(object sender, EventArgs e)
+    private void textStart_TextChanged(object sender, EventArgs e)
+    {
+        OnTextChanged(textStart, value =>
         {
-            UpdateText(null);
-        }
+            if (radioSNES.Checked)
+                value = data.ConvertSnesToPc(value);
 
-        private void radioROM_CheckedChanged(object sender, EventArgs e)
-        {
-            UpdateText(null);
-        }
+            StartRomOffset = value;
+            Count = EndRomOffset - StartRomOffset;
+        });
+    }
 
-        private void textCount_TextChanged(object sender, EventArgs e)
-        {
-            OnTextChanged(textCount, value =>
-            {
-                Count = value;
-                EndRomOffset = StartRomOffset + Count;
-            });
-        }
+    private void OnTextChanged(TextBox textBox, Action<int> OnResult)
+    {
+        if (updatingText)
+            return;
 
-        private void textEnd_TextChanged(object sender, EventArgs e)
-        {
-            OnTextChanged(textEnd, value =>
-            {
-                if (radioSNES.Checked)
-                    value = data.ConvertSnesToPc(value);
+        updatingText = true;
+        var style = radioDec.Checked ? NumberStyles.Number : NumberStyles.HexNumber;
 
-                EndRomOffset = value;
-                Count = EndRomOffset - StartRomOffset;
-            });
-        }
+        if (int.TryParse(textBox.Text, style, null, out var result))
+            OnResult(result);
 
-        private void textStart_TextChanged(object sender, EventArgs e)
-        {
-            OnTextChanged(textStart, value =>
-            {
-                if (radioSNES.Checked)
-                    value = data.ConvertSnesToPc(value);
+        UpdateText(textBox);
+    }
 
-                StartRomOffset = value;
-                Count = EndRomOffset - StartRomOffset;
-            });
-        }
+    private void go_Click(object sender, EventArgs e)
+    {
+        DialogResult = DialogResult.OK;
+    }
 
-        private void OnTextChanged(TextBox textBox, Action<int> OnResult)
-        {
-            if (updatingText)
-                return;
-
-            updatingText = true;
-            var style = radioDec.Checked ? NumberStyles.Number : NumberStyles.HexNumber;
-
-            if (int.TryParse(textBox.Text, style, null, out var result))
-                OnResult(result);
-
-            UpdateText(textBox);
-        }
-
-        private void go_Click(object sender, EventArgs e)
-        {
-            DialogResult = DialogResult.OK;
-        }
-
-        private void cancel_Click(object sender, EventArgs e)
-        {
-            Close();
-        }
+    private void cancel_Click(object sender, EventArgs e)
+    {
+        Close();
     }
 }
