26e16b7bf2ffe3c67aa53fbafa28a7311aed84ff
Dominic Cerquetti <binary1230@users.noreply.github.com>
2025-05-24 12:06:35 -0400

    interfaces: simplify some interfaces - a lot of these were for refactoring that's now run its course - I'm also probably giving up on directly integrating Diz3, we'll just redo that functionality piecemeal if needed

--- Diz.Controllers/Diz.Controllers/src/interfaces/IControllers.cs
+++ Diz.Controllers/Diz.Controllers/src/interfaces/IControllers.cs
@@ -6,148 +6,116 @@ using Diz.Core;
 using Diz.Core.commands;
 using Diz.Core.export;
 using Diz.Core.model;
-using Diz.Core.model.snes;
 using Diz.Core.util;
 using Diz.Cpu._65816;
-using JetBrains.Annotations;
 
 // NOTE: lots of these interfaces were created temporarily for major refactoring.
 // when that process is finished, we should probably take a pass here to simplify anything
 // that ended up being unnecessary or over-complicated
 
-namespace Diz.Controllers.interfaces
+namespace Diz.Controllers.interfaces;
+
+public interface ICloseHandler
 {
-    public interface IController
-    {
-        
-    }
+    public event EventHandler Closed;
+}
 
-    public interface ICloseHandler
-    {
-        public event EventHandler Closed;
-    }
-
-    public interface IFormController : IController, ICloseHandler, IShowable
-    {
-        
-    }
-
-    public interface IDataController : IController
-    {
-        Data Data { get; }
-    }
+public interface IStartFormController : IFormViewer
+{
+    public IStartFormViewer View { get; }
+}
     
-    #if DIZ_3_BRANCH
-    public interface IBytesGridDataController<TRow, TItem> : IDataController, INotifyPropertyChanged
-    {
-        IBytesGridViewer<TItem> ViewGrid { get; set; } 
-        DataSubsetWithSelection<TRow, TItem> DataSubset { get; }
-        void MatchCachedRowsToView();
-    }
-    #endif
+public interface IProjectController : 
+    ITraceLogImporters,
+    IFixInstructionUtils,
+    IFixMisalignedFlags
+{
+    // diz3.0 is going to need some major surgery from this one.
 
-    public interface IStartFormController : IFormController
-    {
-        public IStartFormViewer View { get; }
-    }
-
-    public interface IProjectOpener
-    {
-        public void OpenFileWithNewView(string filename);
-        public void OpenNewViewOfLastLoadedProject();
-    }
-    
-    public interface IProjectController : 
-        ITraceLogImporters, 
-        IFixInstructionUtils, 
-        IFixMisalignedFlags
-    {
-        // diz3.0 is going to need some major surgery from this one.
-
-        public Project Project { get; }
+    public Project Project { get; }
         
-        IViewFactory ViewFactory { get; }
+    IViewFactory ViewFactory { get; }
         
-        public class ProjectChangedEventArgs
+    public class ProjectChangedEventArgs
+    {
+        public enum ProjectChangedType
         {
-            public enum ProjectChangedType
-            {
-                Invalid,
-                Saved,
-                Opened,
-                Imported,
-                Closing
-            }
-
-            public ProjectChangedType ChangeType;
-            public Project Project;
-            public string Filename;
+            Invalid,
+            Saved,
+            Opened,
+            Imported,
+            Closing
         }
+
+        public ProjectChangedType ChangeType;
+        public Project Project;
+        public string Filename;
+    }
                 
-        delegate void ProjectChangedEvent(object sender, ProjectChangedEventArgs e);
-        event ProjectChangedEvent ProjectChanged;
+    delegate void ProjectChangedEvent(object sender, ProjectChangedEventArgs e);
+    event ProjectChangedEvent ProjectChanged;
 
-        IProjectView ProjectView { get; set; }
+    IProjectView ProjectView { get; set; }
 
-        bool OpenProject(string filename);  // older signature
-        string SaveProject(string filename); // older signature. new should return void
+    bool OpenProject(string filename);  // older signature
+    string SaveProject(string filename); // older signature. new should return void
 
-        bool ImportRomAndCreateNewProject(string romFilename);
-        void ImportLabelsCsv(ILabelEditorView labelEditor, bool replaceAll);
-        void SelectOffset(int offset, ISnesNavigation.HistoryArgs historyArgs = null);
+    bool ImportRomAndCreateNewProject(string romFilename);
+    void ImportLabelsCsv(ILabelEditorView labelEditor, bool replaceAll);
+    void SelectOffset(int offset, ISnesNavigation.HistoryArgs historyArgs = null);
 
-        bool ConfirmSettingsThenExportAssembly();
-        bool ExportAssemblyWithCurrentSettings();
+    bool ConfirmSettingsThenExportAssembly();
+    bool ExportAssemblyWithCurrentSettings();
 
-        void MarkChanged(); // rename to MarkUnsaved or similar in Diz3.0
-    }
+    void MarkChanged(); // rename to MarkUnsaved or similar in Diz3.0
+}
     
-    public interface IProjectOpenerHandler : ILongRunningTaskHandler
-    {
-        public void OnProjectOpenSuccess(string filename, Project project);
-        public void OnProjectOpenWarnings(IReadOnlyList<string> warnings);
-        public void OnProjectOpenFail(string fatalError);
-        public string AskToSelectNewRomFilename(string error);
+public interface IProjectOpenerHandler : ILongRunningTaskHandler
+{
+    public void OnProjectOpenSuccess(string filename, Project project);
+    public void OnProjectOpenWarnings(IReadOnlyList<string> warnings);
+    public void OnProjectOpenFail(string fatalError);
+    public string AskToSelectNewRomFilename(string error);
         
-        Project OpenProject(string filename, bool showPopupAlertOnLoaded);
-    }
+    Project OpenProject(string filename, bool showPopupAlertOnLoaded);
+}
 
-    public interface IExportDisassembly
-    {
-        void UpdateExportSettings(LogWriterSettings selectedSettings);
-        void WriteAssemblyOutput();
-    }
+public interface IExportDisassembly
+{
+    void UpdateExportSettings(LogWriterSettings selectedSettings);
+    void WriteAssemblyOutput();
+}
 
-    public interface IFixInstructionUtils
-    {
-        // probably combine this with something else.
-        // not sure this should really be an interface but...
-        bool RescanForInOut();
-    }
+public interface IFixInstructionUtils
+{
+    // probably combine this with something else.
+    // not sure that this should really be an interface but...
+    bool RescanForInOut();
+}
 
-    public interface ITraceLogImporters
-    {
-        void ImportBizHawkCdl(string filename);
-        long ImportBsnesUsageMap(string fileName);
-        long ImportBsnesTraceLogs(string[] fileNames);
-    }
+public interface ITraceLogImporters
+{
+    void ImportBizHawkCdl(string filename);
+    long ImportBsnesUsageMap(string fileName);
+    long ImportBsnesTraceLogs(string[] fileNames);
+}
 
-    public interface IProjectNavigation
-    {
-        public int SelectedSnesOffset { get; set; }
+public interface IProjectNavigation
+{
+    public int SelectedSnesOffset { get; set; }
 
-        void GoTo(int offset);
-        void GoToUnreached(bool end, bool direction);
-        void GoToIntermediateAddress(int offset);
-        // void OnUserChangedSelection(ByteEntry newSelection);
-    }
+    void GoTo(int offset);
+    void GoToUnreached(bool end, bool direction);
+    void GoToIntermediateAddress(int offset);
+    // void OnUserChangedSelection(ByteEntry newSelection);
+}
 
-    public interface ILabelImporter
-    {
-        void ImportLabelsCsv(ILabelEditorView labelEditor, bool replaceAll);
-    }
+public interface ILabelImporter
+{
+    void ImportLabelsCsv(ILabelEditorView labelEditor, bool replaceAll);
+}
 
-    #if DIZ_3_BRANCH
+#if DIZ_3_BRANCH
     public interface IMainFormController : 
         
         IFormController,
@@ -169,41 +137,40 @@ namespace Diz.Controllers.interfaces
         
         void SetProject(string filename, Project project);
     }
-    #endif
+#endif
 
-    public interface IMarkManyController<out TDataSource> : IController
-    {
-        IDataRange DataRange { get; }
-        TDataSource Data { get; }
-        MarkCommand GetMarkCommand();
-    }
+public interface IMarkManyController<out TDataSource>
+{
+    IDataRange DataRange { get; }
+    TDataSource Data { get; }
+    MarkCommand GetMarkCommand();
+}
 
-    public interface ILogCreatorSettingsEditorController : IController, ICloseHandler, INotifyPropertyChangedExt
-    {
-        ILogCreatorSettingsEditorView View { get; set; }
+public interface ILogCreatorSettingsEditorController : INotifyPropertyChangedExt
+{
+    ILogCreatorSettingsEditorView View { get; set; }
     
-        LogWriterSettings Settings { get; set; }
+    LogWriterSettings Settings { get; set; }
 
-        public string KeepPathsRelativeToThisPath { get; set; }
+    public string KeepPathsRelativeToThisPath { get; set; }
 
-        bool PromptSetupAndValidateExportSettings();
+    bool PromptSetupAndValidateExportSettings();
 
-        bool EnsureSelectRealOutputDirectory(bool forcePrompt = false);
-        string GetSampleOutput();
-    }
+    bool EnsureSelectRealOutputDirectory(bool forcePrompt = false);
+    string GetSampleOutput();
+}
     
     
-    public interface IDizAppSettings : INotifyPropertyChanged
-    {
-        string LastProjectFilename { get; set; }
-        bool OpenLastFileAutomatically { get; set; }
-        string LastOpenedFile { get; set; }
-    }
+public interface IDizAppSettings : INotifyPropertyChanged
+{
+    string LastProjectFilename { get; set; }
+    bool OpenLastFileAutomatically { get; set; }
+    string LastOpenedFile { get; set; }
+}
 
-    public interface IDizDocument : INotifyPropertyChanged
-    {
-        Project Project { get; set; }
-        string LastProjectFilename { get; set; }
-        public BindingList<NavigationEntry> NavigationHistory { get; set; }
-    }
+public interface IDizDocument : INotifyPropertyChanged
+{
+    Project Project { get; set; }
+    string LastProjectFilename { get; set; }
+    public BindingList<NavigationEntry> NavigationHistory { get; set; }
 }
--- Diz.Controllers/Diz.Controllers/src/interfaces/IViewers.cs
+++ Diz.Controllers/Diz.Controllers/src/interfaces/IViewers.cs
@@ -5,98 +5,82 @@ using Diz.Core.commands;
 using Diz.Core.Interfaces;
 using JetBrains.Annotations;
 
-namespace Diz.Controllers.interfaces
+namespace Diz.Controllers.interfaces;
+
+public interface IFormViewer : ICloseHandler, IShowable
 {
-    public interface IViewer
-    {
+    
+}
 
-    }
+public interface IShowable
+{
+    void Show();
+}
+    
+public interface IFocusable
+{
+    void BringFormToTop(); // not the most elegant thing
+}
 
-    public interface IFormViewer : IViewer, ICloseHandler, IShowable
-    {
+public interface IStartFormViewer : IProjectOpenRequester, IFormViewer
+{
         
-    }
+}
 
-    public interface IShowable
-    {
-        void Show();
-    }
+public record ProjectOpenEventArgs
+{
+    public string Filename { get; init; }
+    public bool OpenLast { get; init; }
+}
     
-    public interface IFocusable
-    {
-        void BringFormToTop(); // not the most elegant thing
-    }
+public interface IProjectOpenRequester
+{
+    public event EventHandler<ProjectOpenEventArgs> ProjectOpenRequested;
+}
 
-    public interface IStartFormViewer : IProjectOpenRequester, IFormViewer
-    {
+public interface ICloseable
+{
+    void Close();
+}
+
+public interface IModalDialog
+{
+    /// <summary>
+    /// Show the dialog to the user and wait for them to complete
+    /// the steps on the view
+    /// </summary>
+    /// <returns>True if steps were completed and we have a valid result</returns>
+    bool PromptDialog();
+}
+    
+
+public interface IProgressView : IFormViewer, ICloseable, IModalDialog, IProgress<int> {
+    public bool IsMarquee { get; set; }
+    public string TextOverride { get; set; }
+    bool Visible { get; set; }
         
-    }
+    /// <summary>
+    /// Signal that a job (potentially running in another task/thread) has completed.
+    /// CAUTION: Implementers should use thread-safety measures, this may be called
+    /// from a different thread than any other calls 
+    /// </summary>
+    void SignalJobIsDone();
+}
 
-    public record ProjectOpenEventArgs
-    {
-        public string Filename { get; init; }
-        public bool OpenLast { get; init; }
-    }
-    
-    public interface IProjectOpenRequester
-    {
-        public event EventHandler<ProjectOpenEventArgs> ProjectOpenRequested;
-    }
+// diz2 version (use it)
+public interface IMarkManyView<TDataSource> : IModalDialog 
+    where TDataSource : IRomSize
+{
+    MarkCommand.MarkManyProperty Property { get; set; }
+    object GetPropertyValue();
+    [CanBeNull] IMarkManyController<TDataSource> Controller { get; set; }
 
-    public interface ICloseable
-    {
-        void Close();
-    }
+    void AttemptSetSettings(Dictionary<MarkCommand.MarkManyProperty, object> settings);
+    Dictionary<MarkCommand.MarkManyProperty, object> SaveCurrentSettings();
+}
 
-    public interface IModalDialog
-    {
-        /// <summary>
-        /// Show the dialog to the user and wait for them to complete
-        /// the steps on the view
-        /// </summary>
-        /// <returns>True if steps were completed and we have a valid result</returns>
-        bool PromptDialog();
-    }
-    
 
-    public interface IProgressView : IFormViewer, ICloseable, IModalDialog, IProgress<int> {
-        public bool IsMarquee { get; set; }
-        public string TextOverride { get; set; }
-        bool Visible { get; set; }
-        
-        /// <summary>
-        /// Signal that a job (potentially running in another task/thread) has completed.
-        /// CAUTION: Implementers should use thread-safety measures, this may be called
-        /// from a different thread than any other calls 
-        /// </summary>
-        void SignalJobIsDone();
-    }
-    #if DIZ_3_BRANCH
-    
-    // diz3 version (actually... dont use it)
-    public interface IMarkManyView : IViewer, IModalDialog
-    {
-        int Property { get; }
-        int Column { set; } // TODO: make enum with different types supported.
-        IMarkManyController Controller { get; set; }
-        object GetFinalValue();
-    }
-    
-    #else
-    // diz2 version (use it)
-    public interface IMarkManyView<TDataSource> : IViewer, IModalDialog 
-        where TDataSource : IRomSize
-    {
-        MarkCommand.MarkManyProperty Property { get; set; }
-        object GetPropertyValue();
-        [CanBeNull] IMarkManyController<TDataSource> Controller { get; set; }
-
-        void AttemptSetSettings(Dictionary<MarkCommand.MarkManyProperty, object> settings);
-        Dictionary<MarkCommand.MarkManyProperty, object> SaveCurrentSettings();
-    }
-    #endif
-
-    #if DIZ_3_BRANCH
+#if DIZ_3_BRANCH
     public interface IBytesGridViewer<TByteItem> : IRowBaseViewer<TByteItem>, IViewer
     {
         public List<TByteItem> DataSource { get; set; }
@@ -118,54 +102,53 @@ namespace Diz.Controllers.interfaces
 
         public event SelectedOffsetChange SelectedOffsetChanged;
     }
-    #endif
+#endif
     
-    public interface ILabelEditorView : IFormViewer, IFocusable
-    {
-        [CanBeNull] public IProjectController ProjectController { get; set; }
+public interface ILabelEditorView : IFormViewer, IFocusable
+{
+    [CanBeNull] public IProjectController ProjectController { get; set; }
         
-        string PromptForCsvFilename();
-        void RepopulateFromData();
-        void ShowLineItemError(string exMessage, int errLine);
-        void RebindProject();
-    }
+    string PromptForCsvFilename();
+    void RepopulateFromData();
+    void ShowLineItemError(string exMessage, int errLine);
+    void RebindProject();
+}
     
-    public interface IImportRomDialogView
-    {
-        IImportRomDialogController Controller { get; set; }
-        public List<string> EnabledVectorTableEntries { get; }
+public interface IImportRomDialogView
+{
+    IImportRomDialogController Controller { get; set; }
+    public List<string> EnabledVectorTableEntries { get; }
         
-        bool ShowAndWaitForUserToConfirmSettings();
-        void RefreshUi();
-    }
+    bool ShowAndWaitForUserToConfirmSettings();
+    void RefreshUi();
+}
 
-    public interface ICommonGui
-    {
-        bool PromptToConfirmAction(string msg);
+public interface ICommonGui
+{
+    bool PromptToConfirmAction(string msg);
         
-        void ShowError(string msg);
-        void ShowWarning(string msg);
-        void ShowMessage(string msg);
-    }
+    void ShowError(string msg);
+    void ShowWarning(string msg);
+    void ShowMessage(string msg);
+}
     
-    public interface ILogCreatorSettingsEditorView : IViewer, ICloseHandler
-    {
-        ILogCreatorSettingsEditorController Controller { get; set; }
+public interface ILogCreatorSettingsEditorView : IFormViewer
+{
+    ILogCreatorSettingsEditorController Controller { get; set; }
     
-        [CanBeNull] string PromptForLogPathFromFileOrFolderDialog(bool askForFile);
-        bool PromptCreatePath(string buildFullOutputPath, string extraMsg);
+    [CanBeNull] string PromptForLogPathFromFileOrFolderDialog(bool askForFile);
+    bool PromptCreatePath(string buildFullOutputPath, string extraMsg);
         
-        /// <summary>
-        /// Main method, return true if we showed the dialog and edited successfully.
-        /// </summary>
-        /// <returns></returns>
-        bool PromptEditAndConfirmSettings();
-    }
+    /// <summary>
+    /// Main method, return true if we showed the dialog and edited successfully.
+    /// </summary>
+    /// <returns></returns>
+    bool PromptEditAndConfirmSettings();
+}
     
-    #if DIZ_3_BRANCH
+#if DIZ_3_BRANCH
     public interface IDataGridEditorForm : IFormViewer, IProjectView
     {
         IMainFormController MainFormController { get; set; }
     }
-    #endif
-}
+#endif
