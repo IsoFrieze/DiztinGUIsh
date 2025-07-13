1666458cada441695c95ace4a8b0e58b247aa3a4
Dominic Cerquetti <binary1230@users.noreply.github.com>
2025-07-13 14:24:34 -0400

    app version: improve displayed text, show in titlebar

--- Diz.App.Winforms/AppVersionInfo.cs
+++ Diz.App.Winforms/AppVersionInfo.cs
@@ -1,37 +1,40 @@
-﻿using Diz.Core.Interfaces;
+﻿using System.Diagnostics.CodeAnalysis;
+using Diz.Core.Interfaces;
 using Diz.Core.serialization.xml_serializer;
 
 namespace Diz.App.Winforms;
 
+[SuppressMessage("ReSharper", "ClassNeverInstantiated.Global")]
 public class AppVersionInfo : IAppVersionInfo
 {
+    private readonly string versionStr;
+    private readonly string fullBuildDescription;
+    
+    public AppVersionInfo()
+    {
+        // cache these, they won't change
+        versionStr = ThisAssembly.Git.Tag;
+        fullBuildDescription =
+            $"Build info:\r\n------------\r\n" +
+            $"Version: {versionStr}\r\n" +
+            $"Git branch: {ThisAssembly.Git.Branch}\r\n" +
+            $"Git commit: {ThisAssembly.Git.Commit}\r\n" +
+            $"Git repo URL: {ThisAssembly.Git.RepositoryUrl}\r\n" +
+            $"Git tag: {ThisAssembly.Git.Tag}\r\n" +
+            $"Git last commit date: {ThisAssembly.Git.CommitDate}\r\n" +
+            $"Git IsDirty: {ThisAssembly.Git.IsDirtyString}\r\n" +
+            $"Git Commits on top of base: {ThisAssembly.Git.Commits}\r\n" +
+            "\r\n\r\n" +
+            $"Diz app savefile format ver: {ProjectXmlSerializer.LatestSaveFormatVerion}";
+    }
+    
+    [SuppressMessage("ReSharper", "HeuristicUnreachableCode")]
     public string GetVersionInfo(IAppVersionInfo.AppVersionInfoType type)
     {
-        var versionStr = ThisAssembly.Git.Tag;
-        if (string.IsNullOrWhiteSpace(versionStr))
-        {
-            versionStr = $"commit-{ThisAssembly.Git.Commit}";
-        }
-
-        if (ThisAssembly.Git.IsDirty)
-            versionStr += "-dirty";
-        
         return type switch
         {
-            IAppVersionInfo.AppVersionInfoType.FullDescription =>
-                $"Build info:\r\n------------\r\n" +
-                $"Version: {versionStr}\r\n" +
-                $"Git branch: {ThisAssembly.Git.Branch}\r\n" +
-                $"Git commit: {ThisAssembly.Git.Commit}\r\n" +
-                $"Git repo URL: {ThisAssembly.Git.RepositoryUrl}\r\n" +
-                $"Git tag: {ThisAssembly.Git.Tag}\r\n" +
-                $"Git last commit date: {ThisAssembly.Git.CommitDate}\r\n" +
-                $"Git IsDirty: {ThisAssembly.Git.IsDirtyString}\r\n" +
-                $"Git Commits on top of base: {ThisAssembly.Git.Commits}\r\n" +
-                "\r\n\r\n" +
-                $"Diz app savefile format ver: {ProjectXmlSerializer.LatestSaveFormatVerion}",
-            IAppVersionInfo.AppVersionInfoType.Version =>
-                $"{versionStr}",
+            IAppVersionInfo.AppVersionInfoType.FullDescription => fullBuildDescription,
+            IAppVersionInfo.AppVersionInfoType.Version => versionStr,
             _ => ""
         };
     }
