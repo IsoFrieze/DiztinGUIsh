bb377213ec7358916b9a5ceeb3c1b58e86774c29
Dominic Cerquetti <binary1230@users.noreply.github.com>
2025-07-17 14:49:40 -0400

    debug: add native crash handlers - Rider doesn't catch these weird winforms exceptions. if in doubt, enable during debugging. major sledgehammer though, try and keep disabled normally

--- Diz.App.Winforms/Program.cs
+++ Diz.App.Winforms/Program.cs
@@ -1,6 +1,12 @@
 ﻿#nullable enable
 
+// #define DEBUG_EXTRA_CRASH_HANDLING // for catching really stubborn crashes, like in databinding
+
 using System;
+#if DEBUG_EXTRA_CRASH_HANDLING
+using System.IO;
+using System.Windows.Forms;
+#endif
 using Diz.App.Common;
 
 namespace Diz.App.Winforms;
@@ -10,6 +16,21 @@ internal static class Program
     [STAThread]
     private static void Main(string[] args)
     {
+        #if DEBUG_EXTRA_CRASH_HANDLING
+        Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException); // dangerous
+        Application.ThreadException += (sender, e) => 
+        {
+            File.WriteAllText("crash_log.txt", $"Thread Exception: {e.Exception}");
+            MessageBox.Show($"Thread Exception: {e.Exception}");
+        };
+    
+        AppDomain.CurrentDomain.UnhandledException += (sender, e) => 
+        {
+            File.WriteAllText("crash_log.txt", $"Unhandled Exception: {e.ExceptionObject}");
+            MessageBox.Show($"Unhandled Exception: {e.ExceptionObject}");
+        };
+        #endif
+        
         var serviceFactory = DizWinformsRegisterServices.CreateServiceFactoryAndRegisterTypes();
         DizAppCommon.StartApp(serviceFactory, args);
     }
